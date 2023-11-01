40a97d02764c33325d84ab6cb230b1412365fed9
ihromant <vespertilion@gmail.com>
2023-11-01 18:31:51 +0200

    Tests refactoring, additions

--- src/test/java/ua/ihromant/mathutils/BibdPointTest.java
+++ src/test/java/ua/ihromant/mathutils/BibdPointTest.java
@@ -8,89 +8,61 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class BibdPointTest {
     @Test
-    public void testCorrectness() {
-        for (int p : BibdPoint.points()) {
-            assertEquals(6, BibdPoint.point(p).cardinality());
-        }
-        for (int l : BibdPoint.lines()) {
-            assertEquals(3, BibdPoint.line(l).cardinality());
-        }
-        for (int p1 : BibdPoint.points()) {
-            for (int p2 : BibdPoint.points()) {
-                if (p1 != p2) {
-                    BitSet line = BibdPoint.line(BibdPoint.line(p1, p2));
-                    assertTrue(line.get(p1));
-                    assertTrue(line.get(p2));
-                }
-            }
-        }
-        for (int p : BibdPoint.points()) {
-            for (int l : BibdPoint.lines(p)) {
-                assertTrue(BibdPoint.line(l).get(p));
-            }
-        }
-        for (int l : BibdPoint.lines()) {
-            for (int p : BibdPoint.points(l)) {
-                assertTrue(BibdPoint.point(p).get(l));
-            }
-        }
-    }
-
-    @Test
-    public void testHyperbolicity() {
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-        for (int l : BibdPoint.lines()) {
-            BitSet line = BibdPoint.line(l);
-            for (int p : BibdPoint.points()) {
-                if (line.get(p)) {
-                    continue;
-                }
-                int counter = 0;
-                for (int parallel : BibdPoint.lines(p)) {
-                    if (BibdPoint.intersection(parallel, l) == -1) {
-                        counter++;
-                    }
-                }
-                //assertEquals(1, counter);
-                max = Math.max(max, counter);
-                min = Math.min(min, counter);
-            }
-        }
-        System.out.println(min + " " + max);
-    }
-
-    @Test
     public void findQuadruples() {
-        int SIZE = 41;
-        Map<Quadruple, BitSet> sets = getQuadruples(SIZE);
-        System.out.println(sets.size());
+        int size = 41;
+        Map<Quadruple, BitSet> sets = getQuadruples(size);
         List<Map.Entry<Quadruple, BitSet>> entries = new ArrayList<>(sets.entrySet());
         for (int a = 0; a < entries.size(); a++) {
+            BitSet aSet = entries.get(a).getValue();
             for (int b = a + 1; b < entries.size(); b++) {
-                BitSet bs = new BitSet();
-                bs.or(entries.get(a).getValue());
-                bs.or(entries.get(b).getValue());
-                if (bs.cardinality() == SIZE / 2) {
-                    System.out.println(entries.get(a) + " " + entries.get(b));
+                BitSet bSet = entries.get(b).getValue();
+                if (aSet.intersects(bSet)) {
+                    continue;
                 }
+                System.out.println(entries.get(a) + " " + entries.get(b));
             }
         }
     }
 
     @Test
     public void findQuadruples2() {
-        int size = 61;
+        int size = 71;
         Map<Quadruple, BitSet> sets = getQuadruples(size);
-        System.out.println(sets.size());
+        Map<Quintuple, BitSet> quins = getQuintuples(size);
+        List<Map.Entry<Quadruple, BitSet>> entries = new ArrayList<>(sets.entrySet());
+        List<Map.Entry<Quintuple, BitSet>> quinList = new ArrayList<>(quins.entrySet());
+        for (int a = 0; a < quinList.size(); a++) {
+            if (a % 100 == 0) {
+                System.out.println(a);
+            }
+            BitSet aSet = quinList.get(a).getValue();
+            for (int b = a + 1; b < entries.size(); b++) {
+                BitSet bSet = entries.get(b).getValue();
+                if (aSet.intersects(bSet)) {
+                    continue;
+                }
+                for (int c = b + 1; c < entries.size(); c++) {
+                    BitSet cSet = entries.get(c).getValue();
+                    if (cSet.intersects(aSet) || cSet.intersects(bSet)) {
+                        continue;
+                    }
+                    System.out.println(quinList.get(a) + " " + entries.get(b) + " " + entries.get(c));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void findQuadruples1() {
+        int size = 81;
+        Map<Quadruple, BitSet> sets = getQuadruples(size);
         List<Map.Entry<Quadruple, BitSet>> entries = new ArrayList<>(sets.entrySet());
         for (int a = 0; a < entries.size(); a++) {
-            System.out.println(a);
+            if (a % 1000 == 0) {
+                System.out.println(a);
+            }
             BitSet aSet = entries.get(a).getValue();
             for (int b = a + 1; b < entries.size(); b++) {
                 BitSet bSet = entries.get(b).getValue();
@@ -99,32 +71,38 @@ public class BibdPointTest {
                 }
                 for (int c = b + 1; c < entries.size(); c++) {
                     BitSet cSet = entries.get(c).getValue();
-                    if (aSet.intersects(cSet) || bSet.intersects(cSet)) {
+                    if (cSet.intersects(aSet) || cSet.intersects(bSet)) {
                         continue;
                     }
-                    System.out.println(entries.get(a) + " " + entries.get(b) + " " + entries.get(c));
+                    for (int d = c + 1; d < entries.size(); d++) {
+                        BitSet dSet = entries.get(d).getValue();
+                        if (dSet.intersects(cSet) || dSet.intersects(bSet) || dSet.intersects(aSet)) {
+                            continue;
+                        }
+                        System.out.println(entries.get(a) + " " + entries.get(b) + " " + entries.get(c) + " " + entries.get(d));
+                    }
                 }
             }
         }
     }
 
-    private static Map<Quadruple, BitSet> getQuadruples(int SIZE) {
+    private static Map<Quadruple, BitSet> getQuadruples(int size) {
         Map<Quadruple, BitSet> sets = new HashMap<>();
-        for (int i = 1; i < SIZE; i++) {
-            for (int j = i + 1; j < SIZE; j++) {
-                for (int k = j + 1; k < SIZE; k++) {
-                    for (int l = k + 1; l < SIZE; l++) {
+        for (int i = 1; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                for (int k = j + 1; k < size; k++) {
+                    for (int l = k + 1; l < size; l++) {
                         BitSet bs = new BitSet();
-                        bs.set(diff(0, i, SIZE));
-                        bs.set(diff(0, j, SIZE));
-                        bs.set(diff(0, k, SIZE));
-                        bs.set(diff(0, l, SIZE));
-                        bs.set(diff(i, k, SIZE));
-                        bs.set(diff(i, j, SIZE));
-                        bs.set(diff(j, k, SIZE));
-                        bs.set(diff(i, l, SIZE));
-                        bs.set(diff(j, l, SIZE));
-                        bs.set(diff(k, l, SIZE));
+                        bs.set(diff(0, i, size));
+                        bs.set(diff(0, j, size));
+                        bs.set(diff(0, k, size));
+                        bs.set(diff(0, l, size));
+                        bs.set(diff(i, k, size));
+                        bs.set(diff(i, j, size));
+                        bs.set(diff(j, k, size));
+                        bs.set(diff(i, l, size));
+                        bs.set(diff(j, l, size));
+                        bs.set(diff(k, l, size));
                         if (bs.cardinality() == 10) {
                             sets.put(new Quadruple(i, j, k, l), bs);
                         }
@@ -132,6 +110,7 @@ public class BibdPointTest {
                 }
             }
         }
+        System.out.println("Quadruples for size " + size + " count " + sets.size());
         return sets;
     }
 
@@ -184,42 +163,46 @@ public class BibdPointTest {
                 }
             }
         }
+        System.out.println("Triples for size " + size + " count " + sets.size());
         return sets;
     }
 
     @Test
     public void findQuintuples3() {
-        int size = 151;
+        int size = 61;
         Map<Quintuple, BitSet> sets = getQuintuples(size);
-        System.out.println(sets.size());
         List<Map.Entry<Quintuple, BitSet>> entries = new ArrayList<>(sets.entrySet());
         for (int a = 0; a < entries.size(); a++) {
-            System.out.println(a);
+            if (a % 1000 == 0) {
+                System.out.println(a);
+            }
             BitSet aSet = entries.get(a).getValue();
             for (int b = a + 1; b < entries.size(); b++) {
                 BitSet bSet = entries.get(b).getValue();
                 if (aSet.intersects(bSet)) {
                     continue;
                 }
-                for (int c = b + 1; c < entries.size(); c++) {
-                    BitSet cSet = entries.get(c).getValue();
-                    if (aSet.intersects(cSet) || bSet.intersects(cSet)) {
-                        continue;
-                    }
-                    for (int d = c + 1; d < entries.size(); d++) {
-                        BitSet dSet = entries.get(d).getValue();
-                        if (aSet.intersects(dSet) || bSet.intersects(dSet) || cSet.intersects(dSet)) {
-                            continue;
-                        }
-                        for (int e = d + 1; e < entries.size(); e++) {
-                            BitSet eSet = entries.get(e).getValue();
-                            if (aSet.intersects(eSet) || bSet.intersects(eSet) || cSet.intersects(eSet) || dSet.intersects(eSet)) {
-                                continue;
-                            }
-                            System.out.println(entries.get(a) + " " + entries.get(b) + " " + entries.get(c) + " " + entries.get(d) + " " + entries.get(e));
-                        }
-                    }
+                System.out.println(entries.get(a) + " " + entries.get(b));
+            }
+        }
+    }
+
+    @Test
+    public void findSextuple() {
+        int size = 85;
+        Map<Sextuple, BitSet> sets = getSextuples(size);
+        List<Map.Entry<Sextuple, BitSet>> entries = new ArrayList<>(sets.entrySet());
+        for (int a = 0; a < entries.size(); a++) {
+            if (a % 100 == 0) {
+                System.out.println(a);
+            }
+            BitSet aSet = entries.get(a).getValue();
+            for (int b = a + 1; b < entries.size(); b++) {
+                BitSet bSet = entries.get(b).getValue();
+                if (aSet.intersects(bSet)) {
+                    continue;
                 }
+                System.out.println(entries.get(a) + " " + entries.get(b));
             }
         }
     }
@@ -247,7 +230,7 @@ public class BibdPointTest {
                             bs.set(diff(j, m, size));
                             bs.set(diff(k, m, size));
                             bs.set(diff(l, m, size));
-                            if (bs.cardinality() == size / 10) {
+                            if (bs.cardinality() == 15) {
                                 sets.put(new Quintuple(i, j, k, l, m), bs);
                             }
                         }
@@ -255,86 +238,45 @@ public class BibdPointTest {
                 }
             }
         }
+        System.out.println("Quintuples for size " + size + " count " + sets.size());
         return sets;
     }
 
     @Test
     public void findMutant() {
-        int SIZE = 19;
-        Map<Tuple, BitSet> tuples = new HashMap<>();
-        for (int i = 1; i < SIZE; i++) {
-            for (int j = i + 1; j < SIZE; j++) {
-                BitSet bs = new BitSet();
-                bs.set(diff(0, i, SIZE));
-                bs.set(diff(0, j, SIZE));
-                bs.set(diff(i, j, SIZE));
-                if (bs.cardinality() == 3) {
-                    tuples.put(new Tuple(i, j), bs);
+        int size = 73;
+        Map<Quintuple, BitSet> quins = getQuintuples(size);
+        Map<Sextuple, BitSet> six = getSextuples(size);
+        List<Map.Entry<Quintuple, BitSet>> quinList = new ArrayList<>(quins.entrySet());
+        List<Map.Entry<Sextuple, BitSet>> sixList = new ArrayList<>(six.entrySet());
+        for (Map.Entry<Quintuple, BitSet> a1 : quinList) {
+            for (Map.Entry<Sextuple, BitSet> a2 : sixList) {
+                if (a1.getValue().intersects(a2.getValue())) {
+                    continue;
                 }
-            }
-        }
-        Map<Triple, BitSet> triples = new HashMap<>();
-        for (int i = 1; i < SIZE; i++) {
-            for (int j = i + 1; j < SIZE; j++) {
-                for (int k = j + 1; k < SIZE; k++) {
-                    BitSet bs = new BitSet();
-                    bs.set(diff(0, i, SIZE));
-                    bs.set(diff(0, j, SIZE));
-                    bs.set(diff(0, k, SIZE));
-                    bs.set(diff(i, k, SIZE));
-                    bs.set(diff(i, j, SIZE));
-                    bs.set(diff(j, k, SIZE));
-                    if (bs.cardinality() == 6) {
-                        triples.put(new Triple(i, j, k), bs);
-                    }
-                }
-            }
-        }
-        for (Map.Entry<Tuple, BitSet> a1 : tuples.entrySet()) {
-            for (Map.Entry<Triple, BitSet> a2 : triples.entrySet()) {
-                BitSet bs = new BitSet();
-                bs.or(a1.getValue());
-                bs.or(a2.getValue());
-                if (bs.cardinality() == SIZE / 2) {
-                    System.out.println(a1 + " " + a2);
-                }
+                System.out.println(a1 + " " + a2);
             }
         }
     }
 
-    @Test
-    public void findTuples() {
-        int SIZE = 19;
+    private static Map<Tuple, BitSet> getTuples(int size) {
         Map<Tuple, BitSet> sets = new HashMap<>();
-        for (int i = 1; i < SIZE; i++) {
-            for (int j = i + 1; j < SIZE; j++) {
+        for (int i = 1; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
                 BitSet bs = new BitSet();
-                bs.set(diff(0, i, SIZE));
-                bs.set(diff(0, j, SIZE));
-                bs.set(diff(i, j, SIZE));
-                if (bs.cardinality() == SIZE / 6) {
+                bs.set(diff(0, i, size));
+                bs.set(diff(0, j, size));
+                bs.set(diff(i, j, size));
+                if (bs.cardinality() == 3) {
                     sets.put(new Tuple(i, j), bs);
                 }
             }
         }
-        for (Map.Entry<Tuple, BitSet> a1 : sets.entrySet()) {
-            for (Map.Entry<Tuple, BitSet> a2 : sets.entrySet()) {
-                for (Map.Entry<Tuple, BitSet> a3 : sets.entrySet()) {
-                    BitSet bs = new BitSet();
-                    bs.or(a1.getValue());
-                    bs.or(a2.getValue());
-                    bs.or(a3.getValue());
-                    if (bs.cardinality() == SIZE / 2) {
-                        System.out.println(a1 + " " + a2 + " " + a3);
-                    }
-                }
-            }
-        }
+        System.out.println("Tuples for size " + size + " count " + sets.size());
+        return sets;
     }
 
-    @Test
-    public void findSextuples() {
-        int size = 127;
+    private static Map<Sextuple, BitSet> getSextuples(int size) {
         Map<Sextuple, BitSet> sets = new HashMap<>();
         for (int i = 1; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
@@ -364,7 +306,7 @@ public class BibdPointTest {
                                 bs.set(diff(k, n, size));
                                 bs.set(diff(l, n, size));
                                 bs.set(diff(m, n, size));
-                                if (bs.cardinality() == size / 6) {
+                                if (bs.cardinality() == 21) {
                                     sets.put(new Sextuple(i, j, k, l, m, n), bs);
                                 }
                             }
@@ -373,25 +315,8 @@ public class BibdPointTest {
                 }
             }
         }
-        System.out.println(sets.size());
-        List<Map.Entry<Sextuple, BitSet>> entries = new ArrayList<>(sets.entrySet());
-        for (int a = 0; a < entries.size(); a++) {
-            System.out.println(a);
-            BitSet aSet = entries.get(a).getValue();
-            for (int b = a + 1; b < entries.size(); b++) {
-                BitSet bSet = entries.get(b).getValue();
-                if (aSet.intersects(bSet)) {
-                    continue;
-                }
-                for (int c = b + 1; c < entries.size(); c++) {
-                    BitSet cSet = entries.get(c).getValue();
-                    if (aSet.intersects(cSet) || bSet.intersects(cSet)) {
-                        continue;
-                    }
-                    System.out.println(entries.get(a) + " " + entries.get(b) + " " + entries.get(c));
-                }
-            }
-        }
+        System.out.println("Sextuples for size " + size + " count " + sets.size());
+        return sets;
     }
 
     private record Tuple(int i, int j) {}
--- src/test/java/ua/ihromant/mathutils/HyperbolicPlaneTest.java
+++ src/test/java/ua/ihromant/mathutils/HyperbolicPlaneTest.java
@@ -16,13 +16,14 @@ public class HyperbolicPlaneTest {
         HyperbolicPlane fourPoints = new HyperbolicPlane(new int[]{0, 18, 27, 33}, new int[]{0, 7, 24, 36}, new int[]{0, 3, 5, 26});
         HyperbolicPlane otherFourPoints = new HyperbolicPlane(new int[]{0, 33, 34, 39}, new int[]{0, 17, 25, 28}, new int[]{0, 2, 9, 22}, new int[]{0, 19, 23, 37});
         HyperbolicPlane fivePoints = new HyperbolicPlane(new int[]{0, 19, 24, 33, 39}, new int[]{0, 1, 4, 11, 29});
-        HyperbolicPlane otherFivePoints = new HyperbolicPlane(new int[]{0, 17, 18, 21, 45}, new int[]{0, 2, 9, 38, 48}, new int[]{0, 5, 11, 19, 31});
+        HyperbolicPlane otherFivePoints = new HyperbolicPlane(new int[]{0, 16, 17, 31, 35}, new int[]{0, 3, 11, 32, 39});
         HyperbolicPlane triFour = new HyperbolicPlane(new int[]{0, 9, 13}, new int[]{0, 1, 3, 8});
         assertEquals(13, triPoints.pointCount());
         assertEquals(26, triPoints.lineCount());
         testCorrectness(triPoints, of(3), 6);
         testPlayfairIndex(triPoints, of(3));
         testHyperbolicIndex(triPoints, 0, 1);
+        checkPlane(triPoints);
 
         assertEquals(19, otherTriPoints.pointCount());
         assertEquals(57, otherTriPoints.lineCount());
@@ -48,17 +49,63 @@ public class HyperbolicPlaneTest {
         testPlayfairIndex(fivePoints, of(5));
         testHyperbolicIndex(fivePoints, 1, 3);
 
-        assertEquals(61, otherFivePoints.pointCount());
-        assertEquals(183, otherFivePoints.lineCount());
-        testCorrectness(otherFivePoints, of(5), 15);
-        testPlayfairIndex(otherFivePoints, of(10));
-        testHyperbolicIndex(otherFivePoints, 0, 3);
+        assertEquals(41, otherFivePoints.pointCount());
+        assertEquals(82, otherFivePoints.lineCount());
+        testCorrectness(otherFivePoints, of(5), 10);
+        testPlayfairIndex(otherFivePoints, of(5));
+        testHyperbolicIndex(otherFivePoints, 1, 3);
 
         assertEquals(19, triFour.pointCount());
         assertEquals(38, triFour.lineCount());
         testCorrectness(triFour, of(3, 4), 7);
         testPlayfairIndex(triFour, of(3, 4));
         testHyperbolicIndex(triFour, 0, 2);
+    }
+
+    @Test
+    public void testFivePointPlanes() {
+        HyperbolicPlane fp1 = new HyperbolicPlane(new int[]{0, 17, 18, 21, 45}, new int[]{0, 2, 9, 38, 48}, new int[]{0, 5, 11, 19, 31});
+        HyperbolicPlane fp2 = new HyperbolicPlane(new int[]{0, 34, 36, 39, 48}, new int[]{0, 1, 7, 30, 51}, new int[]{0, 18, 26, 42, 46});
+        HyperbolicPlane fp3 = new HyperbolicPlane(new int[]{0, 17, 18, 24, 50}, new int[]{0, 2, 10, 14, 23}, new int[]{0, 3, 19, 34, 39});
+        HyperbolicPlane fp4 = new HyperbolicPlane(new int[]{0, 17, 18, 33, 57}, new int[]{0, 2, 9, 38, 51}, new int[]{0, 20, 26, 31, 34});
+        HyperbolicPlane fp5 = new HyperbolicPlane(new int[]{0, 16, 52, 57, 58}, new int[]{0, 12, 23, 30, 40}, new int[]{0, 14, 22, 46, 48});
+        HyperbolicPlane fp6 = new HyperbolicPlane(new int[]{0, 13, 19, 21, 43, 53}, new int[]{0, 1, 12, 17, 26}, new int[]{0, 3, 7, 36, 51});
+
+        assertEquals(61, fp1.pointCount());
+        assertEquals(183, fp1.lineCount());
+        testCorrectness(fp1, of(5), 15);
+        testPlayfairIndex(fp1, of(10));
+        testHyperbolicIndex(fp1, 0, 3);
+
+        assertEquals(61, fp2.pointCount());
+        assertEquals(183, fp2.lineCount());
+        testCorrectness(fp2, of(5), 15);
+        testPlayfairIndex(fp2, of(10));
+        testHyperbolicIndex(fp2, 0, 3);
+
+        assertEquals(61, fp3.pointCount());
+        assertEquals(183, fp3.lineCount());
+        testCorrectness(fp3, of(5), 15);
+        testPlayfairIndex(fp3, of(10));
+        testHyperbolicIndex(fp3, 0, 3);
+
+        assertEquals(61, fp4.pointCount());
+        assertEquals(183, fp4.lineCount());
+        testCorrectness(fp4, of(5), 15);
+        testPlayfairIndex(fp4, of(10));
+        testHyperbolicIndex(fp4, 0, 3);
+
+        assertEquals(61, fp5.pointCount());
+        assertEquals(183, fp5.lineCount());
+        testCorrectness(fp5, of(5), 15);
+        testPlayfairIndex(fp5, of(10));
+        testHyperbolicIndex(fp5, 0, 3);
+
+        assertEquals(71, fp6.pointCount());
+        assertEquals(213, fp6.lineCount());
+        testCorrectness(fp6, of(5, 6), 16);
+        testPlayfairIndex(fp6, of(10, 11));
+        testHyperbolicIndex(fp6, 0, 4);
     }
 
     private static BitSet of(int... values) {
@@ -93,16 +140,19 @@ public class HyperbolicPlaneTest {
                 assertTrue(plane.point(p).get(l));
             }
         }
-//        for (int x : plane.points()) {
-//            for (int y : plane.points()) {
-//                for (int z : plane.points()) {
-//                    if (plane.collinear(x, y, z)) {
-//                        continue;
-//                    }
-//                    assertEquals(plane.pointCount(), plane.hull(x, y, z).cardinality());
-//                }
-//            }
-//        }
+    }
+
+    private void checkPlane(HyperbolicPlane plane) {
+        for (int x : plane.points()) {
+            for (int y : plane.points()) {
+                for (int z : plane.points()) {
+                    if (plane.collinear(x, y, z)) {
+                        continue;
+                    }
+                    assertEquals(plane.pointCount(), plane.hull(x, y, z).cardinality());
+                }
+            }
+        }
     }
 
     private void testPlayfairIndex(HyperbolicPlane plane, BitSet hyperbolicNumber) {
