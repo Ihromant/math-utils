2703c3176c0b365c38b8845446a83728084faef1
ihromant <vespertilion@gmail.com>
2023-11-07 17:08:57 +0200

    Code for checking Thales and Pappus in different affine-projective spaces

--- src/test/java/ua/ihromant/mathutils/BatchHyperbolicPlaneTest.java
+++ src/test/java/ua/ihromant/mathutils/BatchHyperbolicPlaneTest.java
@@ -15,7 +15,7 @@ import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 import java.util.zip.ZipInputStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BatchHyperbolicPlaneTest {
     private ZipInputStream getZis(InputStream is) throws IOException {
@@ -133,10 +133,12 @@ public class BatchHyperbolicPlaneTest {
     }
 
     @Test
-    public void testProjectiveAndUnitals() throws IOException {
-        try (InputStream is = getClass().getResourceAsStream("/proj/pg216.uni");
+    public void testProjectiveAndUnitals7() throws IOException {
+        String name = "semi4";
+        try (InputStream is = getClass().getResourceAsStream("/proj/" + name + ".uni");
              InputStreamReader isr = new InputStreamReader(Objects.requireNonNull(is));
              BufferedReader br = new BufferedReader(isr)) {
+            System.out.println(name);
             String[] first = br.readLine().trim().split(" ");
             int v = Integer.parseInt(first[0]);
             int b = Integer.parseInt(first[1]);
@@ -144,6 +146,12 @@ public class BatchHyperbolicPlaneTest {
             String next = br.readLine();
             HyperbolicPlane projective = readPlane(v, b, next, br);
             HyperbolicPlaneTest.testCorrectness(projective, of(17), 17);
+            for (int i : projective.lines()) {
+                //if (checkThales(projective, i)) {
+                //    System.out.println("Thales " + i);
+                    System.out.println("Pappus " + i + " " + checkPappus(projective, i));
+                //}
+            }
             int ub = Integer.parseInt(br.readLine());
             br.readLine();
             while ((next = br.readLine()) != null) {
@@ -164,5 +172,115 @@ public class BatchHyperbolicPlaneTest {
                 System.out.println(p.hyperbolicIndex());
             }
         }
+    }
+
+    private boolean checkPappus(HyperbolicPlane p, int dl) {
+        BitSet droppedLine = p.line(dl);
+        for (int o : p.points()) {
+            if (droppedLine.get(o)) {
+                continue;
+            }
+            for (int l1 : p.lines(o)) {
+                for (int l2 : p.lines(o)) {
+                    if (l1 >= l2) {
+                        continue;
+                    }
+                    for (int a1 : p.points(l1)) {
+                        if (a1 == o || droppedLine.get(a1)) {
+                            continue;
+                        }
+                        for (int b1 : p.points(l1)) {
+                            if (b1 == o || a1 == b1 || droppedLine.get(b1)) {
+                                continue;
+                            }
+                            for (int a2 : p.points(l2)) {
+                                if (a2 == o || droppedLine.get(a2)) {
+                                    continue;
+                                }
+                                for (int b2 : p.points(l2)) {
+                                    if (b2 == o || a2 == b2 || droppedLine.get(b2)) {
+                                        continue;
+                                    }
+                                    int a2b1 = p.line(a2, b1);
+                                    int a2b1Infty = p.intersection(a2b1, dl);
+                                    int b2c1 = p.line(b2, a2b1Infty);
+                                    int c1 = p.intersection(b2c1, l1);
+
+                                    int a1b2 = p.line(a1, b2);
+                                    int a1b2Infty = p.intersection(a1b2, dl);
+                                    int b1c2 = p.line(b1, a1b2Infty);
+                                    int c2 = p.intersection(b1c2, l2);
+
+                                    int a1a2 = p.line(a1, a2);
+                                    int c1c2 = p.line(c1, c2);
+                                    if (a1a2 != c1c2 && !droppedLine.get(p.intersection(a1a2, c1c2))) {
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkThales(HyperbolicPlane p, int dl) {
+        BitSet droppedLine = p.line(dl);
+        System.out.println(dl);
+        for (int infty : p.points(dl)) {
+            for (int la : p.lines(infty)) {
+                if (la == dl) {
+                    continue;
+                }
+                for (int lb : p.lines(infty)) {
+                    if (lb == dl || la >= lb) {
+                        continue;
+                    }
+                    for (int lc : p.lines(infty)) {
+                        if (lc == dl || lb >= lc) {
+                            continue;
+                        }
+                        for (int a1 : p.points(la)) {
+                            if (a1 == infty) {
+                                continue;
+                            }
+                            for (int a2 : p.points(la)) {
+                                if (a1 == a2 || a2 == infty) {
+                                    continue;
+                                }
+                                for (int b1 : p.points(lb)) {
+                                    if (b1 == infty) {
+                                        continue;
+                                    }
+                                    for (int b2 : p.points(lb)) {
+                                        if (b1 == b2 || b2 == infty) {
+                                            continue;
+                                        }
+                                        for (int c1 : p.points(lc)) {
+                                            if (c1 == infty) {
+                                                continue;
+                                            }
+                                            for (int c2 : p.points(lc)) {
+                                                if (c1 == c2 || c2 == infty) {
+                                                    continue;
+                                                }
+                                                if (droppedLine.get(p.intersection(p.line(a1, c1), p.line(a2, c2)))
+                                                        && droppedLine.get(p.intersection(p.line(a1, b1), p.line(a2, b2)))
+                                                        && !droppedLine.get(p.intersection(p.line(b1, c1), p.line(b2, c2)))) {
+                                                    return false;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
