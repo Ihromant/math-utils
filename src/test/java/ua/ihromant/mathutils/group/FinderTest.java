7e6285a5552462cbac5335b2f3b98044620cda87
ihromant <vespertilion@gmail.com>
2023-12-17 18:00:23 +0200

    Reworked BIBD finder

--- src/test/java/ua/ihromant/mathutils/group/FinderTest.java
+++ src/test/java/ua/ihromant/mathutils/group/FinderTest.java
@@ -59,7 +59,7 @@ public class FinderTest {
         }
     }
 
-    private void enhanceFrequencies(BitSet[] frequencies, int[] block) {
+    private static void enhanceFrequencies(BitSet[] frequencies, int[] block) {
         for (int i = 0; i < block.length - 1; i++) {
             int t = block[i];
             for (int j = i + 1; j < block.length; j++) {
@@ -107,8 +107,8 @@ public class FinderTest {
 
     @Test
     public void generate3() {
-        int v = 13;
-        int k = 3;
+        int v = 21;
+        int k = 5;
         int r = (v - 1) / (k - 1);
         BitSet[] frequencies = IntStream.range(0, v).mapToObj(i -> new BitSet()).toArray(BitSet[]::new);
         BitSet[] blocks = new BitSet[r + 1];
@@ -120,8 +120,10 @@ public class FinderTest {
         int[] initial = IntStream.range(0, k).map(i -> 1 + (k - 1) * i).toArray();
         enhanceFrequencies(frequencies, initial);
         blocks[r] = of(initial);
-        int[] nextPossible = nextPossible(frequencies, initial, v);
-        designs(v, k, true, nextPossible, blocks, frequencies).forEach(arr -> System.out.println(Arrays.toString(arr)));
+        long time = System.currentTimeMillis();
+        System.out.println(designs(v, k, blocks, v * (v - 1) / k / (k - 1) - r - 1, frequencies)
+                .peek(d -> System.out.println(Arrays.toString(d)))
+                .count() + " " + (System.currentTimeMillis() - time));
     }
 
     private Stream<BitSet[]> designs(int v, int k, boolean parallel, int[] possible, BitSet[] curr, BitSet[] frequencies) {
@@ -155,6 +157,54 @@ public class FinderTest {
                     }
                     designs(v, k, false, nextPossible, nextCurr, nextFrequencies).forEach(sink);
                 });
+    }
+
+    private static Stream<BitSet> blocks(int prev, BitSet curr, int needed, BitSet possible, BitSet[] frequencies) {
+        return possible.stream().boxed().mapMulti((idx, sink) -> {
+            BitSet nextCurr = (BitSet) curr.clone();
+            nextCurr.set(idx);
+            if (needed == 1) {
+                sink.accept(nextCurr);
+                return;
+            }
+            BitSet nextPossible = (BitSet) possible.clone();
+            nextPossible.set(prev + 1, idx + 1, false);
+            BitSet fr = frequencies[idx];
+            for (int i = fr.nextSetBit(idx); i >= 0; i = fr.nextSetBit(i + 1)) {
+                nextPossible.set(i, false);
+            }
+            blocks(prev, nextCurr, needed - 1, nextPossible, frequencies).forEach(sink);
+        });
+    }
+
+    private static Stream<BitSet[]> designs(int variants, int k, BitSet[] curr, int needed, BitSet[] frequencies) {
+        int blockNeeded = k - 1;
+        int cl = curr.length;
+        BitSet prev = curr[cl - 1];
+        int prevFst = prev.nextSetBit(0);
+        int fst = IntStream.range(prevFst, variants - blockNeeded).filter(i -> frequencies[i].cardinality() + 1 != variants).findAny().orElse(variants);
+        BitSet base = of(fst);
+        BitSet possible = (BitSet) frequencies[fst].clone();
+        if (prevFst == fst) {
+            int second = prev.nextSetBit(fst + 1);
+            possible.set(0, second + 1, false);
+            possible.flip(second + 1, variants);
+        } else {
+            possible.set(0, fst, false);
+            possible.flip(fst + 1, variants);
+        }
+        return blocks(fst, base, blockNeeded, possible, frequencies).mapMulti((block, sink) -> {
+            BitSet[] nextCurr = new BitSet[cl + 1];
+            System.arraycopy(curr, 0, nextCurr, 0, cl);
+            nextCurr[cl] = block;
+            if (needed == 1) {
+                sink.accept(nextCurr);
+                return;
+            }
+            BitSet[] nextFrequencies = Arrays.stream(frequencies).map(bs -> (BitSet) bs.clone()).toArray(BitSet[]::new);
+            block.stream().forEach(u -> block.stream().filter(t -> t != u).forEach(t -> nextFrequencies[u].set(t)));
+            designs(variants, k, nextCurr, needed - 1, nextFrequencies).forEach(sink);
+        });
     }
 
     @Test
