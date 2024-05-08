ee2c152bf1ba1fc617825afbf69c9dca37463f21
ihromant <vespertilion@gmail.com>
2024-05-08 21:42:42 +0300

    Precalculated everything needed for vf2

--- src/main/java/ua/ihromant/mathutils/vf2/LinerWrapper.java
+++ src/main/java/ua/ihromant/mathutils/vf2/LinerWrapper.java
@@ -2,31 +2,50 @@ package ua.ihromant.mathutils.vf2;
 
 import ua.ihromant.mathutils.Liner;
 
-public record LinerWrapper(Liner liner) implements Graph {
+public class LinerWrapper implements Graph {
+    private final int size;
+    private final boolean[][] incidence;
+    private final int[][] adjacency;
+
+    public LinerWrapper(Liner liner) {
+        this.size = liner.pointCount() + liner.lineCount();
+        this.incidence = new boolean[size][size];
+        this.adjacency = new int[size][];
+        for (int p = 0; p < liner.pointCount(); p++) {
+            int[] beam = liner.point(p);
+            int[] adj = new int[beam.length];
+            for (int i = 0; i < beam.length; i++) {
+                int l = beam[i];
+                adj[i] = l + liner.pointCount();
+            }
+            adjacency[p] = adj;
+        }
+        for (int l = 0; l < liner.lineCount(); l++) {
+            int[] line = liner.line(l);
+            int[] adj = new int[line.length];
+            int lIdx = l + liner.pointCount();
+            for (int i = 0; i < line.length; i++) {
+                int p = line[i];
+                adj[i] = p;
+                incidence[p][lIdx] = true;
+                incidence[lIdx][p] = true;
+            }
+            adjacency[lIdx] = adj;
+        }
+    }
+
     @Override
     public boolean contains(int from, int to) {
-        if (from < liner.pointCount()) {
-            return to >= liner.pointCount() && liner.flag(to - liner.pointCount(), from);
-        } else {
-            return to < liner.pointCount() && liner.flag(from - liner.pointCount(), to);
-        }
+        return incidence[from][to];
     }
 
     @Override
     public int[] getNeighbors(int vertex) {
-        if (vertex < liner.pointCount()) {
-            int[] beams = liner.lines(vertex).clone();
-            for (int i = 0; i < beams.length; i++) {
-                beams[i] = beams[i] + liner.pointCount();
-            }
-            return beams;
-        } else {
-            return liner.line(vertex - liner.pointCount());
-        }
+        return adjacency[vertex];
     }
 
     @Override
     public int order() {
-        return liner.pointCount() + liner.lineCount();
+        return size;
     }
 }
