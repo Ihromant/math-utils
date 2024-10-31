f8a33045c9be22afe3a900a5e3ececd962114d8c
ihromant <vespertilion@gmail.com>
2024-10-31 15:54:21 +0200

    Improvement

--- src/main/java/ua/ihromant/mathutils/FuzzyLiner.java
+++ src/main/java/ua/ihromant/mathutils/FuzzyLiner.java
@@ -41,28 +41,44 @@ public class FuzzyLiner {
         if (i == j) {
             throw new IllegalArgumentException(i + " " + j);
         }
-        boolean res = !d[i][j];
+        if (d[i][j]) {
+            return false;
+        }
         d[i][j] = true;
         d[j][i] = true;
-        return res;
+        return true;
     }
 
     public boolean colline(int a, int b, int c) {
         if (a == b || a == c || b == c || t[a][b][c]) {
             throw new IllegalArgumentException(a + " " + b + " " + c);
         }
-        boolean res = !l[a][b][c];
+        if (l[a][b][c]) {
+            return false;
+        }
         l[a][b][c] = true;
-        return res;
+        l[a][c][b] = true;
+        l[b][a][c] = true;
+        l[b][c][a] = true;
+        l[c][a][b] = true;
+        l[c][b][a] = true;
+        return true;
     }
 
     public boolean triangle(int a, int b, int c) {
         if (a == b || a == c || b == c || l[a][b][c]) {
             throw new IllegalArgumentException(a + " " + b + " " + c);
         }
-        boolean res = !t[a][b][c];
+        if (t[a][b][c]) {
+            return false;
+        }
         t[a][b][c] = true;
-        return res;
+        t[a][c][b] = true;
+        t[b][a][c] = true;
+        t[b][c][a] = true;
+        t[c][a][b] = true;
+        t[c][b][a] = true;
+        return true;
     }
 
     public boolean collinear(int a, int b, int c) {
