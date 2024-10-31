3e1ec94d41c48283cd2424f68e5cac4b50eb888c
ihromant <vespertilion@gmail.com>
2024-10-31 16:17:45 +0200

    Working Fuzzy Liner

--- src/main/java/ua/ihromant/mathutils/FuzzyLiner.java
+++ src/main/java/ua/ihromant/mathutils/FuzzyLiner.java
@@ -18,9 +18,9 @@ public class FuzzyLiner {
     public FuzzyLiner(int[][] lines) {
         this(Arrays.stream(lines).mapToInt(l -> Arrays.stream(l).max().orElseThrow()).max().orElseThrow() + 1);
         for (int[] line : lines) {
-            for (int i = 0; i < l.length; i++) {
+            for (int i = 0; i < line.length; i++) {
                 int p1 = line[i];
-                for (int j = i + 1; j < l.length; j++) {
+                for (int j = i + 1; j < line.length; j++) {
                     int p2 = line[j];
                     for (int k = 0; k < pc; k++) {
                         if (k == p1 || k == p2) {
@@ -29,7 +29,7 @@ public class FuzzyLiner {
                         if (Arrays.binarySearch(line, k) >= 0) {
                             colline(p1, p2, k);
                         } else {
-                            triangle(p1, p2, k);
+                            triangule(p1, p2, k);
                         }
                     }
                 }
@@ -65,7 +65,7 @@ public class FuzzyLiner {
         return true;
     }
 
-    public boolean triangle(int a, int b, int c) {
+    public boolean triangule(int a, int b, int c) {
         if (a == b || a == c || b == c || l[a][b][c]) {
             throw new IllegalArgumentException(a + " " + b + " " + c);
         }
@@ -81,8 +81,16 @@ public class FuzzyLiner {
         return true;
     }
 
+    public boolean distinct(int a, int b) {
+        return d[a][b];
+    }
+
     public boolean collinear(int a, int b, int c) {
         return l[a][b][c];
+    }
+
+    public boolean triangle(int a, int b, int c) {
+        return t[a][b][c];
     }
 
     public int intersection(Pair fst, Pair snd) {
@@ -92,5 +100,51 @@ public class FuzzyLiner {
             }
         }
         return -1;
+    }
+
+    public void update() {
+        int counter = 0;
+        while (updateStep()) {
+            System.out.println("Updating... " + counter);
+        }
+    }
+
+    private boolean updateStep() {
+        boolean result = false;
+        for (int x = 0; x < pc; x++) {
+            for (int y = x + 1; y < pc; y++) {
+                for (int z = y + 1; z < pc; z++) {
+                    boolean cxyz = collinear(x, y, z);
+                    if (triangle(x, y, z)) {
+                        result = result | distinguish(x, y) | distinguish(y, z) | distinguish(x, z);
+                    }
+                    if (!cxyz) {
+                        continue;
+                    }
+                    result = result | distinguish(x, y) | distinguish(y, z) | distinguish(x, z);
+                    for (int w = z + 1; w < pc; w++) {
+                        if (distinct(z, w) && collinear(x, y, w)) {
+                            result = result | colline(z, w, x) | colline(z, w, y);
+                        }
+                        if (distinct(y, w) && collinear(x, z, w)) {
+                            result = result | colline(y, w, x) | colline(y, w, z);
+                        }
+                        if (distinct(x, w) && collinear(z, y, w)) {
+                            result = result | colline(x, w, y) | colline(x, w, z);
+                        }
+                        if (triangle(x, y, w)) {
+                            result = result | triangule(x, z, w) | triangule(y, z, w);
+                        }
+                        if (triangle(x, z, w)) {
+                            result = result | triangule(x, y, w) | triangule(y, z, w);
+                        }
+                        if (triangle(y, z, w)) {
+                            result = result | triangule(x, z, w) | triangule(x, y, w);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
---
+++ src/test/java/ua/ihromant/mathutils/plane/FuzzyLinerTest.java
@@ -0,0 +1,23 @@
+package ua.ihromant.mathutils.plane;
+
+import org.junit.jupiter.api.Test;
+import ua.ihromant.mathutils.FuzzyLiner;
+
+public class FuzzyLinerTest {
+    @Test
+    public void testFanoNotMoufang() {
+        FuzzyLiner base = new FuzzyLiner(new int[][]{
+                {0, 1, 2},
+                {0, 3, 4},
+                {0, 5, 6},
+                {0, 7, 8, 10, 11},
+                {1, 3, 7},
+                {1, 5, 8},
+                {2, 4, 7},
+                {2, 6, 8},
+                {3, 5, 9, 10},
+                {4, 6, 9, 11}
+        });
+        base.update();
+    }
+}
