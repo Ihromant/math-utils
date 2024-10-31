bdab3644ffa900b2a8c242c8d63ab7f672ad444f
ihromant <vespertilion@gmail.com>
2024-10-31 18:17:31 +0200

    Connect empty quadrangles

--- src/test/java/ua/ihromant/mathutils/plane/FuzzyLinerTest.java
+++ src/test/java/ua/ihromant/mathutils/plane/FuzzyLinerTest.java
@@ -24,6 +24,87 @@ public class FuzzyLinerTest {
         base.update();
         enhanceFullFano(base);
         System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+        base = connectTwos(base);
+        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+    }
+
+    @Test
+    public void testFanoNotMoufang() {
+        FuzzyLiner base = new FuzzyLiner(new int[][]{
+                {0, 1, 2},
+                {0, 3, 4},
+                {0, 5, 6},
+                {0, 7, 8, 10, 11},
+                {1, 3, 7},
+                {1, 5, 8},
+                {2, 4, 7},
+                {2, 6, 8},
+                {3, 5, 9, 10},
+                {4, 6, 9, 11}
+        });
+        base.update();
+        enhanceFullFano(base);
+        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+        base = connectTwos(base);
+        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+        base = connectOnes(base);
+        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+        Quad q;
+        while ((q = base.quad(0)) != null) {
+            int newPt = base.getPc();
+            System.out.println("Adding three points " + newPt + " " + (newPt + 1) + " for quadruple " + q);
+            base = base.addPoint().addPoint().addPoint();
+            base.colline(q.a(), q.b(), newPt);
+            base.colline(q.c(), q.d(), newPt);
+            base.colline(q.a(), q.c(), newPt + 1);
+            base.colline(q.b(), q.d(), newPt + 1);
+            base.colline(q.a(), q.d(), newPt + 2);
+            base.colline(q.b(), q.c(), newPt + 2);
+            base.colline(newPt, newPt + 1, newPt + 2);
+            base = connectTwos(base);
+        }
+    }
+
+    private static FuzzyLiner connectOnes(FuzzyLiner base) {
+        Quad q1;
+        while ((q1 = base.quad(1)) != null) {
+            int newPt = base.getPc();
+            System.out.println("Adding two points " + newPt + " " + (newPt + 1) + " for quadruple " + q1);
+            base = base.addPoint().addPoint();
+            Pair ab = new Pair(q1.a(), q1.b());
+            Pair cd = new Pair(q1.c(), q1.d());
+            int abcd = base.intersection(ab, cd);
+            Pair ac = new Pair(q1.a(), q1.c());
+            Pair bd = new Pair(q1.b(), q1.d());
+            int acbd = base.intersection(ac, bd);
+            Pair ad = new Pair(q1.a(), q1.d());
+            Pair bc = new Pair(q1.b(), q1.c());
+            int adbc = base.intersection(ad, bc);
+            if (abcd >= 0) {
+                base.colline(q1.a(), q1.c(), newPt);
+                base.colline(q1.b(), q1.d(), newPt);
+                base.colline(q1.a(), q1.d(), newPt + 1);
+                base.colline(q1.b(), q1.c(), newPt + 1);
+                base.colline(abcd, newPt, newPt + 1);
+            } else {
+                base.colline(q1.a(), q1.b(), newPt);
+                base.colline(q1.c(), q1.d(), newPt);
+                if (acbd >= 0) {
+                    base.colline(q1.a(), q1.d(), newPt + 1);
+                    base.colline(q1.b(), q1.c(), newPt + 1);
+                    base.colline(acbd, newPt, newPt + 1);
+                } else {
+                    base.colline(q1.a(), q1.c(), newPt + 1);
+                    base.colline(q1.b(), q1.d(), newPt + 1);
+                    base.colline(adbc, newPt, newPt + 1);
+                }
+            }
+            base = connectTwos(base);
+        }
+        return base;
+    }
+
+    private static FuzzyLiner connectTwos(FuzzyLiner base) {
         Quad q2;
         while ((q2 = base.quad(2)) != null) {
             int newPt = base.getPc();
@@ -53,60 +134,10 @@ public class FuzzyLinerTest {
                     base.colline(abcd, acbd, newPt);
                 }
             }
-            base.update();
+            enhanceFullFano(base);
         }
-        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
-    }
-
-    @Test
-    public void testFanoNotMoufang() {
-        FuzzyLiner base = new FuzzyLiner(new int[][]{
-                {0, 1, 2},
-                {0, 3, 4},
-                {0, 5, 6},
-                {0, 7, 8, 10, 11},
-                {1, 3, 7},
-                {1, 5, 8},
-                {2, 4, 7},
-                {2, 6, 8},
-                {3, 5, 9, 10},
-                {4, 6, 9, 11}
-        });
-        base.update();
         enhanceFullFano(base);
-        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
-        Quad q;
-        while ((q = base.quad(2)) != null) {
-            int newPt = base.getPc();
-            System.out.println("Adding point " + newPt + " to almost full quad " + q);
-            base = base.addPoint();
-            Pair ab = new Pair(q.a(), q.b());
-            Pair cd = new Pair(q.c(), q.d());
-            int abcd = base.intersection(ab, cd);
-            Pair ac = new Pair(q.a(), q.c());
-            Pair bd = new Pair(q.b(), q.d());
-            int acbd = base.intersection(ac, bd);
-            Pair ad = new Pair(q.a(), q.d());
-            Pair bc = new Pair(q.b(), q.c());
-            int adbc = base.intersection(ad, bc);
-            if (abcd < 0) {
-                base.colline(q.a(), q.b(), newPt);
-                base.colline(q.c(), q.d(), newPt);
-                base.colline(acbd, adbc, newPt);
-            } else {
-                if (acbd < 0) {
-                    base.colline(q.a(), q.c(), newPt);
-                    base.colline(q.b(), q.d(), newPt);
-                    base.colline(abcd, adbc, newPt);
-                } else {
-                    base.colline(q.a(), q.d(), newPt);
-                    base.colline(q.b(), q.c(), newPt);
-                    base.colline(abcd, acbd, newPt);
-                }
-            }
-            base.update();
-        }
-        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+        return base;
     }
 
     private static void enhanceFullFano(FuzzyLiner fl) {
@@ -123,9 +154,7 @@ public class FuzzyLinerTest {
                     fl.colline(abcd, acbd, adbc);
                 }
             }
-            if (incorrect) {
-                fl.update();
-            }
+            fl.update();
         }
     }
 }
