ba70b962770cfceefb314666f96e083805b3e8a2
ihromant <vespertilion@gmail.com>
2024-11-03 15:21:15 +0200

    Enhancement

--- src/test/java/ua/ihromant/mathutils/plane/FuzzySepLinerTest.java
+++ src/test/java/ua/ihromant/mathutils/plane/FuzzySepLinerTest.java
@@ -2,7 +2,10 @@ package ua.ihromant.mathutils.plane;
 
 import org.junit.jupiter.api.Test;
 import ua.ihromant.mathutils.FuzzySepLiner;
+import ua.ihromant.mathutils.Pair;
 import ua.ihromant.mathutils.Triple;
+
+import java.util.List;
 
 public class FuzzySepLinerTest {
     @Test
@@ -23,6 +26,33 @@ public class FuzzySepLinerTest {
                 new Triple(0, 7, 9)});
         System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
         FuzzySepLiner next = base.intersectLines();
-        System.out.println(next.getPc());
+        System.out.println(next.getD().size() + " " + next.getL().size() + " " + next.getT().size() + " " + (next.getL().size() + next.getT().size()));
+        next = enhanceFullFano(next);
+        System.out.println(next.getD().size() + " " + next.getL().size() + " " + next.getT().size() + " " + (next.getL().size() + next.getT().size()));
+        base = next.subLiner(10);
+        System.out.println(base.getD().size() + " " + base.getL().size() + " " + base.getT().size() + " " + (base.getL().size() + base.getT().size()));
+    }
+
+    public FuzzySepLiner enhanceFullFano(FuzzySepLiner liner) {
+        boolean incorrect = true;
+        while (incorrect) {
+            incorrect = false;
+            List<Quad> full = liner.quads(3);
+            System.out.println("Quads " + full.size());
+            for (Quad q : full) {
+                int abcd = liner.intersection(new Pair(q.a(), q.b()), new Pair(q.c(), q.d()));
+                int acbd = liner.intersection(new Pair(q.a(), q.c()), new Pair(q.b(), q.d()));
+                int adbc = liner.intersection(new Pair(q.a(), q.d()), new Pair(q.b(), q.c()));
+                if (!liner.collinear(abcd, acbd, adbc)) {
+                    incorrect = true;
+                    liner.colline(abcd, acbd, adbc);
+                }
+            }
+            liner.update();
+            System.out.println("Before " + liner.getPc());
+            liner = liner.quotient();
+            System.out.println("After " + liner.getPc());
+        }
+        return liner;
     }
 }
