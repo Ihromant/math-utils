105a99f09dd27d5f4ca7744795c5a35e1034c7b0
ihromant <vespertilion@gmail.com>
2024-11-07 13:11:02 +0200

    AP generator

--- src/test/java/ua/ihromant/mathutils/fuzzy/FuzzyBalLinerTest.java
+++ src/test/java/ua/ihromant/mathutils/fuzzy/FuzzyBalLinerTest.java
@@ -1,7 +1,6 @@
 package ua.ihromant.mathutils.fuzzy;
 
 import org.junit.jupiter.api.Test;
-import ua.ihromant.mathutils.GaloisField;
 import ua.ihromant.mathutils.Liner;
 import ua.ihromant.mathutils.util.FixBS;
 
@@ -34,11 +33,28 @@ public class FuzzyBalLinerTest {
         }
     }
 
+    @Test
+    public void generateAP() {
+        int v = 15;
+        int k = 3;
+        int r = (v - 1) / (k - 1);
+        int b = v * r / k;
+        int[][] lines = beamBlocks(v, k);
+        List<FuzzyBalLiner> liners = List.of(FuzzyBalLiner.of(v, k, lines));
+        int needed = (b - lines.length) * (k - 2);
+        while (needed > 0) {
+            AtomicLong cnt = new AtomicLong();
+            liners = nextStage(v, k, liners, FuzzyBalLinerTest::updateAP, cnt);
+            needed--;
+            System.out.println(needed + " " + liners.size() + " " + cnt.get());
+        }
+    }
+
     private static List<FuzzyBalLiner> nextStage(int v, int k, List<FuzzyBalLiner> partials, Consumer<FuzzyBalLiner> checker, AtomicLong cnt) {
         int r = (v - 1) / (k - 1);
         int allC = r * (k - 1) * (k - 2) / 2;
         int all = (v - 1) * (v - 2) / 2;
-        Map<FixBS, FuzzyBalLiner> nonIso = partials.stream().parallel().<FuzzyBalLiner>mapMulti((lnr, sink) -> {
+        Map<FixBS, FuzzyBalLiner> nonIso = partials.stream()/*.parallel()*/.<FuzzyBalLiner>mapMulti((lnr, sink) -> {
             int[][] chars = lnr.pointChars();
             int min = Integer.MAX_VALUE;
             int pt = -1;
@@ -90,5 +106,66 @@ public class FuzzyBalLinerTest {
 
     private static Liner toLiner(FuzzyBalLiner liner) {
         return new Liner(liner.getV(), liner.lines().stream().map(l -> l.stream().toArray()).toArray(int[][]::new));
+    }
+
+    private static void updateAP(FuzzyBalLiner liner) {
+        int v = liner.getV();
+        while (true) {
+            Queue<Rel> queue = new ArrayDeque<>(v);
+            for (int a = 0; a < v; a++) {
+                for (int b = a + 1; b < v; b++) {
+                    for (int c = b + 1; c < v; c++) {
+                        if (!liner.triangle(a, b, c)) {
+                            continue;
+                        }
+                        for (int d = c + 1; d < v; d++) {
+                            if (!liner.triangle(a, b, d) || !liner.triangle(a, c, d) || !liner.triangle(b, c, d)) {
+                                continue;
+                            }
+                            for (int i = 0; i < v; i++) {
+                                if (liner.collinear(a, b, i) && liner.collinear(c, d, i)) {
+                                    enforceAP(liner, v, a, b, c, d, queue);
+                                }
+                                if (liner.collinear(a, c, i) && liner.collinear(b, d, i)) {
+                                    enforceAP(liner, v, a, c, b, d, queue);
+                                }
+                                if (liner.collinear(a, d, i) && liner.collinear(b, c, i)) {
+                                    enforceAP(liner, v, a, d, b, c, queue);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (queue.isEmpty()) {
+                return;
+            }
+            liner.update(queue);
+        }
+    }
+
+    private static void enforceAP(FuzzyBalLiner liner, int v, int a, int b, int c, int d, Queue<Rel> queue) {
+        for (int j = 0; j < v; j++) {
+            if (liner.collinear(a, d, j)) {
+                if (!liner.triangle(b, c, j)) {
+                    queue.add(new Trg(b, c, j));
+                }
+            }
+            if (liner.collinear(b, c, j)) {
+                if (!liner.triangle(a, d, j)) {
+                    queue.add(new Trg(a, d, j));
+                }
+            }
+            if (liner.collinear(a, c, j)) {
+                if (!liner.triangle(b, d, j)) {
+                    queue.add(new Trg(b, d, j));
+                }
+            }
+            if (liner.collinear(b, d, j)) {
+                if (!liner.triangle(a, c, j)) {
+                    queue.add(new Trg(a, c, j));
+                }
+            }
+        }
     }
 }
