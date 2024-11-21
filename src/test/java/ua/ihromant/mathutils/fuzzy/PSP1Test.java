db0d97b49db01e783be71c8878a55bc601b591a2
ihromant <vespertilion@gmail.com>
2024-11-21 13:41:22 +0200

    Other test

--- src/test/java/ua/ihromant/mathutils/fuzzy/PSP1Test.java
+++ src/test/java/ua/ihromant/mathutils/fuzzy/PSP1Test.java
@@ -9,6 +9,32 @@ import java.util.function.UnaryOperator;
 
 public class PSP1Test {
     @Test
+    public void testP2() {
+        int[][] ps = new int[][]{
+                {0, 1, 2, 3},
+                {0, 4, 5, 6},
+                {1, 6, 7},
+                {2, 5, 7},
+                {3, 4, 7},
+                {2, 6, 8},
+                {3, 5, 8},
+                {1, 4, 8},
+                {1, 5, 9},
+                {2, 4, 9},
+                //{3, 6, 9}
+        };
+        FuzzySLiner base = FuzzySLiner.of(ps, new Triple[]{new Triple(0, 1, 4), new Triple(7, 8, 9)});
+        UnaryOperator<FuzzySLiner> op = lnr -> ContradictionUtil.process(lnr, List.of(ContradictionUtil::processP1S, ContradictionUtil::processD3));
+        base.printChars();
+        base = ContradictionUtil.singleByContradiction(base, false, op);
+        base.printChars();
+        base = base.intersectLines();
+        base.printChars();
+        base = ContradictionUtil.singleByContradiction(base, true, op);
+        base.printChars();
+    }
+
+    @Test
     public void testPS() {
         int[][] ps = new int[][]{
                 {0, 1, 2, 3},
