23c169f5bd382196975e9b94ab34c3923830c8a8
ihromant <vespertilion@gmail.com>
2024-11-23 16:20:57 +0200

    Minor change

--- src/main/java/ua/ihromant/mathutils/plane/AffineChar.java
+++ src/main/java/ua/ihromant/mathutils/plane/AffineChar.java
@@ -2,10 +2,9 @@ package ua.ihromant.mathutils.plane;
 
 import java.util.List;
 
-public record AffineChar(String name, boolean translation, int dl, Characteristic chr, List<TernarMapping> ternars) {
+public record AffineChar(String name, boolean translation, int dl, List<TernarMapping> ternars) {
     @Override
     public String toString() {
-        return "AC(" + name + " " + dl + " " + translation + " " + chr + " " + ternars + ")";
+        return "AC(" + name + " " + dl + " " + translation + " " + ternars + ")";
     }
 }
-
