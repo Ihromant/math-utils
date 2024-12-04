bfaaebaee2401d7e0363c57ba963ffb58b70223d
ihromant <vespertilion@gmail.com>
2024-12-04 16:04:07 +0200

    Proofs reordering

--- src/test/java/ua/ihromant/mathutils/fuzzy/D3P2Test.java
+++ src/test/java/ua/ihromant/mathutils/fuzzy/D3P2Test.java
@@ -134,30 +134,6 @@ public class D3P2Test {
     }
 
     @Test
-    public void testD3P1S() {
-        int[][] d3 = {
-                {0, 1, 2},
-                {0, 3, 4},
-                {0, 5, 6},
-                {1, 3, 6, 7},
-                {1, 4, 5, 8},
-                {2, 4, 7},
-                {2, 6, 8},
-                {2, 3, 5, 9},
-                {4, 6, 9}
-        };
-        FuzzySLiner base = FuzzySLiner.of(d3, new Triple[]{new Triple(1, 3, 5), new Triple(2, 4, 6),
-                new Triple(0, 1, 3), new Triple(0, 1, 5), new Triple(0, 3, 5),
-                new Triple(7, 8, 9)});
-        UnaryOperator<FuzzySLiner> op = lnr -> ContradictionUtil.process(lnr, List.of(ContradictionUtil::processP1S));
-        base.printChars();
-        base = base.intersectLines();
-        base.printChars();
-        base = op.apply(base);
-        base.printChars();
-    }
-
-    @Test
     public void testD3D2S() {
         int[][] d3 = {
                 {0, 1, 2},
@@ -174,6 +150,30 @@ public class D3P2Test {
                 new Triple(0, 1, 3), new Triple(0, 1, 5), new Triple(0, 3, 5),
                 new Triple(7, 8, 9)});
         UnaryOperator<FuzzySLiner> op = lnr -> ContradictionUtil.process(lnr, List.of(ContradictionUtil::processD2S));
+        base.printChars();
+        base = base.intersectLines();
+        base.printChars();
+        base = op.apply(base);
+        base.printChars();
+    }
+
+    @Test
+    public void testD3P1S() {
+        int[][] d3 = {
+                {0, 1, 2},
+                {0, 3, 4},
+                {0, 5, 6},
+                {1, 3, 6, 7},
+                {1, 4, 5, 8},
+                {2, 4, 7},
+                {2, 6, 8},
+                {2, 3, 5, 9},
+                {4, 6, 9}
+        };
+        FuzzySLiner base = FuzzySLiner.of(d3, new Triple[]{new Triple(1, 3, 5), new Triple(2, 4, 6),
+                new Triple(0, 1, 3), new Triple(0, 1, 5), new Triple(0, 3, 5),
+                new Triple(7, 8, 9)});
+        UnaryOperator<FuzzySLiner> op = lnr -> ContradictionUtil.process(lnr, List.of(ContradictionUtil::processP1S));
         base.printChars();
         base = base.intersectLines();
         base.printChars();
@@ -202,22 +202,5 @@ public class D3P2Test {
         base.printChars();
         base = op.apply(base);
         base.printChars();
-        base = ContradictionUtil.singleByContradiction(base, true, op);
-        base.printChars();
-        ContradictionUtil.multipleByContradiction(base, true, op, l -> {
-            try {
-                l.printChars();
-                System.out.println("Found partial");
-                l = ContradictionUtil.singleByContradiction(l, false, op);
-                l.printChars();
-                ContradictionUtil.multipleByContradiction(l, false, op, l1 -> {
-                    l1.printChars();
-                    System.out.println("Found example");
-                });
-            } catch (IllegalArgumentException e) {
-                System.out.println("Exception partial");
-                // ok
-            }
-        });
     }
 }
