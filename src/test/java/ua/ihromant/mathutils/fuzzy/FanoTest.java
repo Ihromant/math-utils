bb8f80c121923078e5aee6c72eb522182a0538f8
ihromant <vespertilion@gmail.com>
2024-12-08 16:49:30 +0200

    Alternative fano

--- src/test/java/ua/ihromant/mathutils/fuzzy/FanoTest.java
+++ src/test/java/ua/ihromant/mathutils/fuzzy/FanoTest.java
@@ -50,4 +50,48 @@ public class FanoTest {
             }
         });
     }
+
+    @Test
+    public void testFano1() {
+        int[][] antiMoufang = {
+                {0, 1, 2},
+                {0, 3, 4},
+                {0, 5, 6},
+                {1, 3, 7},
+                {1, 4, 5, 8},
+                {2, 4, 7},
+                {2, 6, 8},
+                {3, 5, 9},
+                {4, 6, 9}
+        };
+        FuzzySLiner base = FuzzySLiner.of(antiMoufang, new Triple[]{new Triple(1, 3, 5), new Triple(2, 4, 6),
+                new Triple(0, 1, 3), new Triple(0, 1, 5), new Triple(0, 3, 5),
+                new Triple(7, 8, 9)});
+        UnaryOperator<FuzzySLiner> op = lnr -> ContradictionUtil.process(lnr, List.of(ContradictionUtil::processFullFano,
+                ContradictionUtil::processP1, ContradictionUtil::processPS, ContradictionUtil::processD2, ContradictionUtil::processD1S));
+        base.printChars();
+        base = ContradictionUtil.singleByContradiction(base, false, op);
+        base.printChars();
+        base = base.intersectLines();
+        base.printChars();
+        base = op.apply(base);
+        base.printChars();
+        base = ContradictionUtil.singleByContradiction(base, true, op);
+        base.printChars();
+        ContradictionUtil.multipleByContradiction(base, true, op, l -> {
+            try {
+                l.printChars();
+                System.out.println("Found partial");
+                l = ContradictionUtil.singleByContradiction(l, false, op);
+                l.printChars();
+                ContradictionUtil.multipleByContradiction(l, false, op, l1 -> {
+                    l1.printChars();
+                    System.out.println("Found example");
+                });
+            } catch (IllegalArgumentException e) {
+                System.out.println("Exception partial");
+                // ok
+            }
+        });
+    }
 }
