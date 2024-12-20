5180c8e3e4f651c7c0f51c58ad92350f58c25f79
ihromant <vespertilion@gmail.com>
2024-12-20 13:27:10 +0200

    Introduced IntList

---
+++ src/test/java/ua/ihromant/mathutils/vector/IntList.java
@@ -0,0 +1,27 @@
+package ua.ihromant.mathutils.vector;
+
+public class IntList {
+    private final int[] arr;
+    private int size = 0;
+
+    public IntList(int size) {
+        this.arr = new int[size];
+    }
+
+    public IntList(int[] arr) {
+        this.arr = arr;
+        this.size = arr.length;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(int v) {
+        arr[size++] = v;
+    }
+
+    public int get(int idx) {
+        return arr[idx];
+    }
+}
