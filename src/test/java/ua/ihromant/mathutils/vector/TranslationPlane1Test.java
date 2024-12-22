3585adf4f58a6637c58ce90142b0b89bbce68296
ihromant <vespertilion@gmail.com>
2024-12-22 14:00:12 +0200

    Cleanup

--- src/test/java/ua/ihromant/mathutils/vector/TranslationPlane1Test.java
+++ src/test/java/ua/ihromant/mathutils/vector/TranslationPlane1Test.java
@@ -18,16 +18,12 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 public class TranslationPlane1Test {
@@ -58,224 +54,8 @@ public class TranslationPlane1Test {
         }
     }
 
-    private IntList[] readOrbits(int pow) throws IOException {
-        try (InputStream is = new FileInputStream("/home/ihromant/maths/trans/orbits" + pow + ".txt");
-             InputStreamReader isr = new InputStreamReader(Objects.requireNonNull(is));
-             BufferedReader br = new BufferedReader(isr)) {
-            return br.lines().map(line -> {
-                int idx = line.indexOf('[');
-                if (idx < 0) {
-                    return null;
-                }
-                String[] split = line.substring(idx + 1, line.length() - 1).split(", ");
-                return Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
-            }).filter(Objects::nonNull).map(IntList::new).toArray(IntList[]::new);
-        }
-    }
-
-    private int[][] readTuples(int pow) throws IOException {
-        try (InputStream is = new FileInputStream("/home/ihromant/maths/trans/tuples" + pow + ".txt");
-             InputStreamReader isr = new InputStreamReader(Objects.requireNonNull(is));
-             BufferedReader br = new BufferedReader(isr)) {
-            return br.lines().map(line -> {
-                String[] split = line.substring(1, line.length() - 1).split(", ");
-                return Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
-            }).toArray(int[][]::new);
-        }
-    }
-
-    private int[] splitOrder(int[] split) {
-        Integer[] ixs = IntStream.range(0, split.length).boxed().toArray(Integer[]::new);
-        Arrays.sort(ixs, Comparator.comparingInt(i -> -split[i]));
-        return Arrays.stream(ixs).mapToInt(Integer::intValue).toArray();
-    }
-
-    @Test
-    public void testSplits() {
-        int p = 2;
-        int n = 10;
-        int half = n / 2;
-        int pow = LinearSpace.pow(p, half);
-        int sum = pow - 2;
-        List<int[]> tuples = new ArrayList<>();
-        int tl = 8;
-        int[] baseTuple = new int[tl];
-        generate(baseTuple, 0, sum, tuples::add);
-        System.out.println(tuples.size());
-        //tuples.forEach(t -> System.out.println(Arrays.toString(t)));
-        List<int[]> filtered = tuples.stream().filter(t -> t[0] >= t[1]
-                && t[2] >= t[3] && t[2] >= t[4] && t[2] >= t[5] && t[2] >= t[6] && t[2] >= t[7]).toList();
-        System.out.println(filtered.size());
-        filtered.forEach(t -> System.out.println(Arrays.toString(t)));
-    }
-
-    private static void generate(int[] tuple, int idx, int sum, Consumer<int[]> cons) {
-        if (idx == tuple.length) {
-            cons.accept(tuple.clone());
-            return;
-        }
-        for (int i = 0; i <= sum; i++) {
-            tuple[idx] = i;
-            if (idx == tuple.length - 2) {
-                tuple[idx + 1] = sum - i;
-                generate(tuple, tuple.length, 0, cons);
-                continue;
-            }
-            generate(tuple, idx + 1, sum - i, cons);
-        }
-    }
-
     @Test
     public void generate() throws IOException {
-        int p = 2;
-        int n = 10;
-        int half = n / 2;
-        int pow = LinearSpace.pow(p, half);
-        ModuloMatrixHelper helper = ModuloMatrixHelper.of(p, half);
-        IntList[] orbits = readOrbits(pow);
-        int[][] tuples = readTuples(pow);
-
-        LinearSpace mini = LinearSpace.of(p, half);
-        LinearSpace sp = LinearSpace.of(p, n);
-        int sc = sp.cardinality();
-        int mc = mini.cardinality();
-
-        FixBS first = new FixBS(sc);
-        first.set(0, mc);
-        FixBS second = new FixBS(sc);
-        for (int i = 0; i < mc; i++) {
-            second.set(i * mc);
-        }
-        FixBS third = new FixBS(sc);
-        for (int i = 0; i < mc; i++) {
-            third.set(mc * i + i);
-        }
-        FixBS[] base = new FixBS[mc + 1];
-        base[0] = first;
-        base[1] = second;
-        base[2] = third;
-        AtomicInteger counter = new AtomicInteger();
-        Map<Characteristic, List<ProjChar>> projData = TranslationPlaneTest.readKnown(mc);
-        for (int[] tuple : tuples) {
-            int[] splitOrder = splitOrder(tuple);
-            int[] tupleIdx = calcTupleIdx(tuple, splitOrder);
-            BiConsumer<int[], List<Integer>> cons = (arr, vl) -> {
-                FixBS[] newBase = base.clone();
-                for (int i = 0; i < arr.length; i++) {
-                    FixBS ln = new FixBS(sc);
-                    int a = arr[i];
-                    for (int x = 1; x < mc; x++) {
-                        int ax = helper.mulVec(a, x);
-                        ln.set(ax * mc + x);
-                    }
-                    newBase[i + 3] = ln;
-                }
-                int[][] lines = TranslationPlaneTest.toProjective(sp, newBase);
-                Liner l = new Liner(lines.length, lines);
-                if (TranslationPlaneTest.isDesargues(l, mc)) {
-                    System.out.println("Desargues " + Arrays.toString(arr));
-                    return;
-                }
-                ProjChar chr = TranslationPlaneTest.newTranslation(counter.toString(), l, projData);
-                if (projData.values().stream().flatMap(List::stream).noneMatch(pd -> pd == chr)) {
-                    projData.computeIfAbsent(chr.ternars().getFirst().chr(), k -> new ArrayList<>()).add(chr);
-                    counter.incrementAndGet();
-                    System.out.println(chr);
-                    System.out.println(Arrays.toString(arr));
-                } else {
-                    System.out.println("Existing " + chr.name() + " " + Arrays.toString(arr));
-                }
-            };
-            int[] partSpread = new int[mini.cardinality() - 2];
-            tree(helper, orbits, tupleIdx, filterGl(helper, p), Arrays.stream(orbits[tupleIdx[0]].toArray()).boxed().toList(), partSpread, 0, cons);
-            System.out.println(Arrays.toString(tuple));
-        }
-    }
-
-    @Test
-    public void testForIdx() throws IOException {
-        int pow = 16;
-        int sum = pow - 2;
-        int[][] tuples = readTuples(16);
-        IntStream.range(0, 1000).forEach(i -> {
-            int[] tuple = tuples[ThreadLocalRandom.current().nextInt(tuples.length)];
-            int[] order = splitOrder(tuple);
-            int idx = ThreadLocalRandom.current().nextInt(sum);
-            System.out.println(idx + " " + Arrays.toString(tuple) + " " + forIdx(idx, tuple, order));
-        });
-    }
-
-    private static int forIdx(int idx, int[] tuple, int[] tupleOrder) {
-        int acc = 0;
-        for (int i : tupleOrder) {
-            acc = acc + tuple[i];
-            if (acc > idx) {
-                return i;
-            }
-        }
-        throw new IllegalStateException();
-    }
-
-    private static int[] calcTupleIdx(int[] tuple, int[] tupleOrder) {
-        return IntStream.range(0, Arrays.stream(tuple).sum()).map(i -> forIdx(i, tuple, tupleOrder)).toArray();
-    }
-
-    private void tree(ModuloMatrixHelper helper, IntList[] orbits, int[] tupleIdx, IntList subGl, List<Integer> v, int[] partSpread, int idx, BiConsumer<int[], List<Integer>> sink) {
-        int needed = partSpread.length - idx;
-        if (needed == 0) {
-            sink.accept(partSpread, v);
-            return;
-        }
-        FixBS filter = new FixBS(helper.matCount());
-        for (int a : v) {
-            if (filter.get(a)) {
-                continue;
-            }
-            int[] newArr = partSpread.clone();
-            newArr[idx] = a;
-            int orbitIdx = tupleIdx[idx];
-            List<Integer> newV = new ArrayList<>(v.size());
-            boolean last = needed == 1;
-            int nextOrbitIdx = last ? 0 : tupleIdx[idx + 1];
-            if (!last && orbitIdx != nextOrbitIdx) {
-                ex: for (int b : orbits[nextOrbitIdx].toArray()) {
-                    for (int i = 0; i <= idx; i++) {
-                        int el = newArr[i];
-                        if (!helper.hasInv(helper.sub(b, el))) {
-                            continue ex;
-                        }
-                    }
-                    newV.add(b);
-                }
-            } else {
-                for (int b : v) {
-                    if (b > a && helper.hasInv(helper.sub(b, a))) {
-                        newV.add(b);
-                    }
-                }
-            }
-            IntList centralizer = new IntList(subGl.size());
-            for (int i = 0; i < subGl.size(); i++) {
-                int el = subGl.get(i);
-                int invEl = helper.inv(el);
-                int prod = helper.mul(helper.mul(invEl, a), el);
-                filter.set(prod);
-                if (idx == 0) {
-                    filter.set(helper.inv(prod));
-                }
-
-                int lMul = helper.mul(a, el);
-                int rMul = helper.mul(el, a);
-                if (lMul == rMul) {
-                    centralizer.add(el);
-                }
-            }
-            tree(helper, orbits, tupleIdx, centralizer, newV, newArr, idx + 1, sink);
-        }
-    }
-
-    @Test
-    public void generateAlt() throws IOException {
         int p = 2;
         int n = 8;
         int half = n / 2;
@@ -336,7 +116,22 @@ public class TranslationPlane1Test {
                 }
             };
             int[] partSpread = new int[mini.cardinality() - 2];
-            treeAlt(helper, filterGl(helper, p), new State(orbits, partSpread, new int[]{min}, new int[]{0}, 1, 0), cons);
+            tree(helper, filterGl(helper, p), new State(orbits, partSpread, new int[]{min}, new int[]{0}, 1, 0), cons);
+        }
+    }
+
+    private IntList[] readOrbits(int pow) throws IOException {
+        try (InputStream is = new FileInputStream("/home/ihromant/maths/trans/orbits" + pow + ".txt");
+             InputStreamReader isr = new InputStreamReader(Objects.requireNonNull(is));
+             BufferedReader br = new BufferedReader(isr)) {
+            return br.lines().map(line -> {
+                int idx = line.indexOf('[');
+                if (idx < 0) {
+                    return null;
+                }
+                String[] split = line.substring(idx + 1, line.length() - 1).split(", ");
+                return Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+            }).filter(Objects::nonNull).map(IntList::new).toArray(IntList[]::new);
         }
     }
 
@@ -490,7 +285,7 @@ public class TranslationPlane1Test {
         return result;
     }
 
-    private void treeAlt(ModuloMatrixHelper helper, IntList subGl, State state, Callback sink) {
+    private void tree(ModuloMatrixHelper helper, IntList subGl, State state, Callback sink) {
         int idx = state.sum();
         if (state.isFull()) {
             sink.accept(state, subGl);
@@ -525,9 +320,9 @@ public class TranslationPlane1Test {
                     }
                 }
                 if (centralizer.isEmpty()) {
-                    treeAltNoSubGl(helper, next, sink);
+                    treeSimple(helper, next, sink);
                 } else {
-                    treeAlt(helper, centralizer, next, sink);
+                    tree(helper, centralizer, next, sink);
                 }
             }
         }
@@ -559,16 +354,16 @@ public class TranslationPlane1Test {
                         }
                     }
                     if (centralizer.isEmpty()) {
-                        treeAltNoSubGl(helper, next, sink);
+                        treeSimple(helper, next, sink);
                     } else {
-                        treeAlt(helper, centralizer, next, sink);
+                        tree(helper, centralizer, next, sink);
                     }
                 }
             }
         }
     }
 
-    private void treeAltNoSubGl(ModuloMatrixHelper helper, State state, Callback sink) {
+    private void treeSimple(ModuloMatrixHelper helper, State state, Callback sink) {
         if (state.isFull()) {
             sink.accept(state, new IntList(0));
             return;
@@ -580,7 +375,7 @@ public class TranslationPlane1Test {
             IntList v = baseNext.currV();
             for (int j = 0; j < v.size(); j++) {
                 State next = baseNext.addOperatorToSpread(helper, j);
-                treeAltNoSubGl(helper, next, sink);
+                treeSimple(helper, next, sink);
             }
         }
         if (canJump) {
@@ -589,7 +384,7 @@ public class TranslationPlane1Test {
                 IntList v = baseNext.currV();
                 for (int j = 0; j < v.size(); j++) {
                     State next = baseNext.addOperatorToSpread(helper, j);
-                    treeAltNoSubGl(helper, next, sink);
+                    treeSimple(helper, next, sink);
                 }
             }
         }
@@ -656,7 +451,7 @@ public class TranslationPlane1Test {
                     ps.flush();
                 };
                 int[] partSpread = new int[pow - 2];
-                treeAlt(helper, filterGl(helper, p), new State(orbits, partSpread, new int[]{min}, new int[]{0}, 1, 0), cons);
+                tree(helper, filterGl(helper, p), new State(orbits, partSpread, new int[]{min}, new int[]{0}, 1, 0), cons);
             }
         }
     }
@@ -766,7 +561,7 @@ public class TranslationPlane1Test {
                         System.out.println("Existing " + chr.name() + " " + Arrays.toString(arr) + " " + state);
                     }
                 };
-                treeAltNoSubGl(helper, st, cons);
+                treeSimple(helper, st, cons);
                 ps.println(Arrays.toString(start[0]));
                 ps.flush();
             });
