adcb463dfe6e2bfc60706eb1db7134ef98f409fe
ihromant <vespertilion@gmail.com>
2025-02-04 16:37:29 +0200

    Minor change

--- src/test/java/ua/ihromant/mathutils/ApplicatorTest.java
+++ src/test/java/ua/ihromant/mathutils/ApplicatorTest.java
@@ -14,12 +14,12 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicatorTest {
-    private static class SubGroupConfig {
+    private static class SubGroupApplicator {
         private final Group gr;
         private final FixBS[] cosets;
         private final int[] idx;
 
-        public SubGroupConfig(SubGroup sg) {
+        public SubGroupApplicator(SubGroup sg) {
             this.gr = sg.group();
             Set<FixBS> set = new HashSet<>();
             for (int i = 0; i < gr.order(); i++) {
@@ -40,27 +40,24 @@ public class ApplicatorTest {
             }
         }
 
-        private int idx(int g) {
-            return idx[g];
-        }
-
-        private int apply(int g, int el) {
-            return gr.op(g, cosets[el].nextSetBit(0));
+        private int apply(int g, int x) {
+            int min = cosets[x].nextSetBit(0);
+            return idx[gr.op(g, min)];
         }
     }
 
     private static class OrbitApplicator {
-        private final SubGroupConfig[] subs;
+        private final SubGroupApplicator[] subs;
         private final int[] oBeg;
         private final int v;
 
         public OrbitApplicator(SubGroup[] subs) {
-            this.subs = new SubGroupConfig[subs.length];
+            this.subs = new SubGroupApplicator[subs.length];
             this.oBeg = new int[subs.length];
             int min = 0;
             for (int i = 0; i < subs.length; i++) {
                 oBeg[i] = min;
-                SubGroupConfig conf = new SubGroupConfig(subs[i]);
+                SubGroupApplicator conf = new SubGroupApplicator(subs[i]);
                 this.subs[i] = conf;
                 min = min + conf.cosets.length;
             }
@@ -73,13 +70,72 @@ public class ApplicatorTest {
                 idx = -idx - 2;
             }
             int min = oBeg[idx];
-            SubGroupConfig conf = subs[idx];
-            return conf.idx(conf.apply(g, x - min)) + min;
+            SubGroupApplicator conf = subs[idx];
+            return conf.apply(g, x - min) + min;
         }
     }
 
-    private record OrbitState(FixBS block, FixBS stabilizer, FixBS filter, FixBS selfDiff, int size) {
-        private OrbitState acceptElem(OrbitApplicator applicator, FixBS globalFilter, int val, int v, int k) {
+    private record State(FixBS block, FixBS stabilizer, FixBS filter, FixBS selfDiff, int size) {
+        private State acceptElem(Group group, FixBS globalFilter, int val, int v, int k) {
+            FixBS newBlock = block.copy();
+            FixBS queue = new FixBS(v);
+            queue.set(val);
+            int sz = size;
+            FixBS newSelfDiff = selfDiff.copy();
+            FixBS newStabilizer = stabilizer.copy();
+            FixBS newFilter = filter.copy();
+            while (!queue.isEmpty()) {
+                if (++sz > k) {
+                    return null;
+                }
+                int x = queue.nextSetBit(0);
+                if (x < val) {
+                    return null;
+                }
+                FixBS stabExt = new FixBS(v);
+                FixBS selfDiffExt = new FixBS(v);
+                for (int b = newBlock.nextSetBit(0); b >= 0; b = newBlock.nextSetBit(b + 1)) {
+                    int bInv = group.inv(b);
+                    int xInv = group.inv(x);
+                    int xb = group.op(x, bInv);
+                    selfDiffExt.set(xb);
+                    if (newSelfDiff.get(xb) || newBlock.get(group.op(xb, x))) {
+                        stabExt.set(xb);
+                    }
+                    int bx = group.op(b, xInv);
+                    if (newSelfDiff.get(bx)) {
+                        stabExt.set(bx);
+                    }
+                    selfDiffExt.set(bx);
+                    int diff = group.op(bInv, x);
+                    if (globalFilter.get(diff)) {
+                        return null;
+                    }
+                    int outDiff = group.op(xInv, b);
+                    newFilter.set(diff);
+                    if (globalFilter.get(outDiff)) {
+                        return null;
+                    }
+                    newFilter.set(outDiff);
+                }
+                newBlock.set(x);
+                stabExt.andNot(newStabilizer);
+                for (int st = newStabilizer.nextSetBit(1); st >= 0; st = newStabilizer.nextSetBit(st + 1)) {
+                    queue.set(group.op(st, x));
+                }
+                for (int st = stabExt.nextSetBit(1); st >= 0; st = stabExt.nextSetBit(st + 1)) {
+                    for (int b = newBlock.nextSetBit(0); b >= 0; b = newBlock.nextSetBit(b + 1)) {
+                        queue.set(group.op(st, b));
+                    }
+                }
+                newStabilizer.or(stabExt);
+                newSelfDiff.or(selfDiffExt);
+                queue.andNot(newBlock);
+            }
+            return new State(newBlock, newStabilizer, newFilter, newSelfDiff, sz);
+        }
+
+        private State acceptElem(SubGroupApplicator app, FixBS globalFilter, int val, int v, int k) {
             FixBS newBlock = block.copy();
             FixBS queue = new FixBS(v);
             queue.set(val);
@@ -135,7 +191,7 @@ public class ApplicatorTest {
                 newSelfDiff.or(selfDiffExt);
                 queue.andNot(newBlock);
             }
-            return new OrbitState(newBlock, newStabilizer, newFilter, newSelfDiff, sz);
+            return new State(newBlock, newStabilizer, newFilter, newSelfDiff, sz);
         }
     }
 
