bd5ac34eac19c8d0db405d9bea8fc224590bd31e
ihromant <vespertilion@gmail.com>
2025-02-06 18:07:28 +0200

    New methods for IntList

--- src/main/java/ua/ihromant/mathutils/IntList.java
+++ src/main/java/ua/ihromant/mathutils/IntList.java
@@ -15,8 +15,8 @@ public class IntList {
         this.size = arr.length;
     }
 
-    private IntList(int[] arr, int size) {
-        this.arr = arr.clone();
+    public IntList(int[] arr, int size) {
+        this.arr = arr;
         this.size = size;
     }
 
@@ -24,8 +24,66 @@ public class IntList {
         return size;
     }
 
+    // IMPORTANT: unsafe, so use read-only or if you know what are you doing
+    public int[] arr() {
+        return arr;
+    }
+
     public void add(int v) {
         arr[size++] = v;
+    }
+
+    public void addToSorted(int el) {
+        int pos = size;
+        while (pos > 0) {
+            int diff = arr[pos - 1] - el;
+            if (diff > 0) {
+                pos--;
+            } else if (diff == 0) {
+                return;
+            } else {
+                break;
+            }
+        }
+        int sh = size - pos;
+        if (sh > 0) {
+            System.arraycopy(arr, pos, arr, pos + 1, sh);
+        }
+        arr[pos] = el;
+        size++;
+    }
+
+    public void addToSortedDesc(int el) {
+        int pos = size;
+        while (pos > 0) {
+            int diff = arr[pos - 1] - el;
+            if (diff < 0) {
+                pos--;
+            } else if (diff == 0) {
+                return;
+            } else {
+                break;
+            }
+        }
+        int sh = size - pos;
+        if (sh > 0) {
+            System.arraycopy(arr, pos, arr, pos + 1, sh);
+        }
+        arr[pos] = el;
+        size++;
+    }
+
+    public int remove() {
+        return arr[--size];
+    }
+    
+    public boolean contains(int el) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == el) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int get(int idx) {
