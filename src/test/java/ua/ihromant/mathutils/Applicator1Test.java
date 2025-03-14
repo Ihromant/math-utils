03b507c4a998dc96b812ea5b2ef5c846889184e9
ihromant <vespertilion@gmail.com>
2025-03-14 11:55:27 +0200

    WIP

--- src/test/java/ua/ihromant/mathutils/Applicator1Test.java
+++ src/test/java/ua/ihromant/mathutils/Applicator1Test.java
@@ -8,8 +8,15 @@ import ua.ihromant.mathutils.util.FixBS;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Applicator1Test {
     @Test
@@ -56,12 +63,13 @@ public class Applicator1Test {
 
         private State acceptElem(int el, int v) {
             int sz = block.size();
-            block.add(el);
+            IntList nextBlock = block.copy();
+            nextBlock.add(el);
             FixBS newFilter = filter.copy();
             FixBS newWhiteList = whiteList.copy();
             int invEl = v - el;
             for (int i = 0; i < sz; i++) {
-                int val = block.get(i);
+                int val = nextBlock.get(i);
                 int diff = el - val;
                 int outDiff = invEl + val;
                 newFilter.set(diff);
@@ -70,13 +78,13 @@ public class Applicator1Test {
                     newWhiteList.clear((el + outDiff / 2) % v);
                 }
                 for (int j = 0; j <= sz; j++) {
-                    int nv = block.get(j);
+                    int nv = nextBlock.get(j);
                     newWhiteList.clear((nv + diff) % v);
                     newWhiteList.clear((nv + outDiff) % v);
                 }
             }
             newWhiteList.diffModuleShifted(newFilter, v, invEl);
-            return new State(block, newFilter, newWhiteList);
+            return new State(nextBlock, newFilter, newWhiteList);
         }
 
         private int size() {
@@ -97,7 +105,110 @@ public class Applicator1Test {
             }
         }
         int[] multipliers = Combinatorics.multipliers(v);
-        FixBS[] currDes = new FixBS[0];
+        IntList newBlock = new IntList(k);
+        newBlock.add(0);
+        FixBS filter = baseFilter(v, k);
+        FixBS whiteList = filter.copy();
+        whiteList.flip(1, v);
+        Map<List<FixBS>, Liner> liners = new ConcurrentHashMap<>();
+        searchDesigns(new State[0], freq, new State(newBlock, filter, whiteList).acceptElem(1, v), v, k, des -> {
+            if (des[des.length - 1].filter.cardinality() != v - 1) {
+                return false;
+            }
+            FixBS[] base = Arrays.stream(des).map(st -> FixBS.of(v, st.block.toArray())).toArray(FixBS[]::new);
+            for (int mul : multipliers) {
+                if (bigger(base, Arrays.stream(base).map(bl -> minimalTuple(bl, mul, v)).sorted().toArray(FixBS[]::new))) {
+                    return true;
+                }
+            }
+            Liner l = new Liner(v, Arrays.stream(des).flatMap(bl -> blocks(bl.block.toArray(), v)).toArray(int[][]::new));
+            if (liners.putIfAbsent(Arrays.stream(base).toList(), l) == null) {
+                System.out.println(l.hyperbolicFreq() + " " + Arrays.toString(Arrays.stream(des).map(State::block).toArray()));
+            }
+            return true;
+        });
+    }
+
+    private static Stream<int[]> blocks(int[] block, int v) {
+        Set<FixBS> set = new HashSet<>(2 * v);
+        List<int[]> res = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            FixBS fbs = new FixBS(v);
+            for (int el : block) {
+                fbs.set((el + i) % v);
+            }
+            if (set.add(fbs)) {
+                res.add(fbs.toArray());
+            }
+        }
+        return res.stream();
+    }
+
+    private boolean bigger(FixBS[] base, FixBS[] cand) {
+        for (int i = 0; i < base.length; i++) {
+            int cmp = base[i].compareTo(cand[i]);
+            if (cmp < 0) {
+                return false;
+            }
+            if (cmp > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static FixBS baseFilter(int v, int k) {
+        FixBS filter = new FixBS(v);
+        int rest = v % (k * (k - 1));
+        if (rest == k) {
+            for (int i = 1; i < v; i++) {
+                if (i * k % v == 0) {
+                    filter.set(i);
+                }
+            }
+        }
+        if (rest == (k - 1)) {
+            for (int i = 1; i < v; i++) {
+                if (i * (k - 1) % v == 0) {
+                    filter.set(i);
+                }
+            }
+        }
+        return filter;
+    }
+
+    private static void searchDesigns(State[] currDesign, int[] freq, State state, int v, int k, Predicate<State[]> cons) {
+        IntList block = state.block;
+        if (freq[state.size()] > 0) {
+            State[] nextDesign = Arrays.copyOf(currDesign, currDesign.length + 1);
+            nextDesign[currDesign.length] = state;
+            if (cons.test(nextDesign)) {
+                return;
+            }
+            IntList newBlock = new IntList(k);
+            newBlock.add(0);
+            FixBS whiteList = state.filter.copy();
+            whiteList.flip(1, v);
+            State nextState = new State(newBlock, state.filter, whiteList).acceptElem(whiteList.nextSetBit(0), v);
+            int[] newFreq = freq.clone();
+            newFreq[state.size()]--;
+            searchDesigns(nextDesign, newFreq, nextState, v, k, cons);
+        }
+        if (hasNext(freq, state.size() + 1)) {
+            for (int el = state.whiteList.nextSetBit(block.getLast()); el >= 0; el = state.whiteList.nextSetBit(el + 1)) {
+                State nextState = state.acceptElem(el, v);
+                searchDesigns(currDesign, freq, nextState, v, k, cons);
+            }
+        }
+    }
+
+    private static boolean hasNext(int[] freq, int from) {
+        for (int i = from; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static FixBS minimalTuple(FixBS tuple, int multiplier, int v) {
