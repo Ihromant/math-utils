89c1787758cd80912cfe351aec156b68c773c947
ihromant <vespertilion@gmail.com>
2025-06-21 10:28:45 +0300

    Inlined v and k

--- src/test/java/ua/ihromant/mathutils/BibdFinder7Test.java
+++ src/test/java/ua/ihromant/mathutils/BibdFinder7Test.java
@@ -20,23 +20,32 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class BibdFinder7Test {
+    private static final int v = 91;
+    private static final int k = 6;
+    private static final int blocksNeeded = v / k / (k - 1);
+    private static final FixBS baseFilter = baseFilter(v, k);
+    private static final int HALVES_EVEN_OFFSET = 0;
+    private static final int HALVES_ODD_OFFSET = v / 2 + 1;
+    private static final int HALVES_EVEN_SHIFT = v % 2 == 0 ? (v / 2) : 0;
+    private static final int HALVES_ODD_SHIFT = v % 2 == 0 ? 0 : (v / 2 + 1);
+
     private record State(FixBS block, FixBS inv, FixBS halves, FixBS selfSum, FixBS filter, FixBS whiteList, int size) {
-        private static State forBlock(int v, FixBS baseFilter, FixBS block) {
-            State state = next(baseFilter, v);
+        private static State forBlock(FixBS block) {
+            State state = next(baseFilter);
             for (int el = block.nextSetBit(2); el >= 0; el = block.nextSetBit(el + 1)) {
-                state = state.acceptElem(el, v);
+                state = state.acceptElem(el);
             }
             return state;
         }
 
-        private static State next(FixBS filter, int v) {
+        private static State next(FixBS filter) {
             FixBS base = FixBS.of(v, 0);
             FixBS whiteList = filter.copy();
             whiteList.flip(1, v);
-            return new State(base, base, base, base, filter, whiteList, 1).acceptElem(filter.nextClearBit(1), v);
+            return new State(base, base, base, base, filter, whiteList, 1).acceptElem(filter.nextClearBit(1));
         }
 
-        private State acceptElem(int el, int v) {
+        private State acceptElem(int el) {
             int invEl = v - el;
             FixBS nextBlock = block.copy();
             nextBlock.set(el);
@@ -50,14 +59,14 @@ public class BibdFinder7Test {
             newFilter.orModuleShifted(inv, v, invEl);
             newWhiteList.diffModuleShifted(nextSelfSum, v, el);
             FixBS newHalves = halves.copy();
-            newHalves.set((el >>> 1) + (((el & 1) != 0) ? (v >>> 1) + 1 : 0));
-            newWhiteList.diffModuleShifted(halves, v, v - (el >>> 1) - (((el & 1) != 0) ? v % 2 == 0 ? 0 : v / 2 + 1 : v % 2 == 0 ? v / 2 : 0));
+            newHalves.set((el >>> 1) + (((el & 1) != 0) ? HALVES_ODD_OFFSET : HALVES_EVEN_OFFSET));
+            newWhiteList.diffModuleShifted(halves, v, v - (el >>> 1) - (((el & 1) != 0) ? HALVES_ODD_SHIFT : HALVES_EVEN_SHIFT));
             newWhiteList.diffModuleShifted(newFilter, v, invEl);
             return new State(nextBlock, nextInv, newHalves, nextSelfSum, newFilter, newWhiteList, size + 1);
         }
     }
 
-    private static void calcCycles(int v, int k, State[] design, State state, Predicate<State[]> sink) {
+    private static void calcCycles(State[] design, State state, Predicate<State[]> sink) {
         FixBS whiteList = state.whiteList();
         FixBS currBlock = state.block();
         int idx = state.size();
@@ -73,17 +82,17 @@ public class BibdFinder7Test {
         }
         int max = first ? (v + fst - minMidSpace + 1) / 2 : v - currBlock.nextSetBit(fst + 1) + fst - minMidSpace;
         for (int el = whiteList.nextSetBit(lastVal); el >= 0 && el < max; el = whiteList.nextSetBit(el + 1)) {
-            State next = state.acceptElem(el, v);
+            State next = state.acceptElem(el);
             if (next.size() == k) {
                 State[] nextDesign = Arrays.copyOf(design, design.length + 1);
                 nextDesign[design.length] = next;
                 if (sink.test(nextDesign)) {
                     return;
                 }
-                next = State.next(next.filter(), v);
-                calcCycles(v, k, nextDesign, next, sink);
+                next = State.next(next.filter());
+                calcCycles(nextDesign, next, sink);
             } else {
-                calcCycles(v, k, design, next, sink);
+                calcCycles(design, next, sink);
             }
         }
     }
@@ -110,23 +119,19 @@ public class BibdFinder7Test {
 
     @Test
     public void toConsole() throws IOException {
-        Group gr = new CyclicGroup(91);
-        int v = gr.order();
-        int k = 6;
+        Group gr = new CyclicGroup(v);
         File beg = new File("/home/ihromant/maths/diffSets/nbeg", k + "-" + gr.name() + "beg.txt");
         try (FileInputStream allFis = new FileInputStream(beg);
              InputStreamReader allIsr = new InputStreamReader(allFis);
              BufferedReader allBr = new BufferedReader(allIsr)) {
-            FixBS[] arr = allBr.lines().map(l -> readFirst(l, v)).toArray(FixBS[]::new);
-            logResultsDepth(System.out, v, k, arr);
+            FixBS[] arr = allBr.lines().map(BibdFinder7Test::readFirst).toArray(FixBS[]::new);
+            logResultsDepth(System.out, arr);
         }
     }
 
     @Test
     public void toFile() throws IOException {
-        Group gr = new CyclicGroup(156);
-        int v = gr.order();
-        int k = 6;
+        Group gr = new CyclicGroup(v);
         File f = new File("/home/ihromant/maths/diffSets/nbeg", k + "-" + gr.name() + ".txt");
         File beg = new File("/home/ihromant/maths/diffSets/nbeg", k + "-" + gr.name() + "beg.txt");
         try (FileOutputStream fos = new FileOutputStream(f, true);
@@ -138,27 +143,25 @@ public class BibdFinder7Test {
              FileInputStream fis = new FileInputStream(f);
              InputStreamReader isr = new InputStreamReader(fis);
              BufferedReader br = new BufferedReader(isr)) {
-            Set<FixBS> set = allBr.lines().map(l -> readFirst(l, v)).collect(Collectors.toSet());
+            Set<FixBS> set = allBr.lines().map(BibdFinder7Test::readFirst).collect(Collectors.toSet());
             br.lines().forEach(l -> {
                 if (l.contains("[[")) {
                     System.out.println(l);
                 } else {
-                    set.remove(readFirst(l, v));
+                    set.remove(readFirst(l));
                 }
             });
-            logResultsDepth(ps, v, k, set.toArray(FixBS[]::new));
+            logResultsDepth(ps, set.toArray(FixBS[]::new));
         }
     }
 
-    private static FixBS readFirst(String line, int v) {
+    private static FixBS readFirst(String line) {
         return FixBS.of(v, Arrays.stream(line.substring(1, line.length() - 1).split(", ")).mapToInt(Integer::parseInt).toArray());
     }
 
-    private static void logResultsDepth(PrintStream destination, int v, int k, FixBS[] unProcessed) {
+    private static void logResultsDepth(PrintStream destination, FixBS[] unProcessed) {
         System.out.println(v + " " + k);
         System.out.println("Initial size " + unProcessed.length);
-        int blocksNeeded = v / k / (k - 1);
-        FixBS baseFilter = baseFilter(v, k);
         AtomicInteger counter = new AtomicInteger();
         long time = System.currentTimeMillis();
         Predicate<State[]> designConsumer = design -> {
@@ -175,10 +178,10 @@ public class BibdFinder7Test {
         };
         AtomicInteger cnt = new AtomicInteger();
         Arrays.stream(unProcessed).parallel().forEach(init -> {
-            State fst = State.forBlock(v, baseFilter, init);
+            State fst = State.forBlock(init);
             State[] design = new State[]{fst};
-            State initial = State.next(fst.filter(), v);
-            calcCycles(v, k, design, initial, designConsumer);
+            State initial = State.next(fst.filter());
+            calcCycles(design, initial, designConsumer);
             if (destination != System.out) {
                 destination.println(init);
                 destination.flush();
@@ -193,21 +196,17 @@ public class BibdFinder7Test {
 
     @Test
     public void logAllCycles() {
-        Group group = new CyclicGroup(91);
-        int k = 6;
-        logAllCycles(group, k);
+        Group group = new CyclicGroup(v);
+        logAllCycles(group);
     }
 
-    private static void logAllCycles(Group group, int k) {
+    private static void logAllCycles(Group group) {
         System.out.println(group.name() + " " + k);
-        int v = group.order();
-        FixBS filter = baseFilter(v, k);
-        int blocksNeeded = v / k / (k - 1);
         State[] base = new State[0];
-        FixBS whiteList = filter.copy();
+        FixBS whiteList = baseFilter.copy();
         whiteList.flip(1, v);
-        State initial = State.next(filter, v);
-        calcCycles(v, k, base, initial, design -> {
+        State initial = State.next(baseFilter);
+        calcCycles(base, initial, design -> {
             if (design.length < blocksNeeded) {
                 return false;
             }
