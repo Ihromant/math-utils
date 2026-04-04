b3e56db8a2d928644890d15823e3e5d167c1501e
ihromant <vespertilion@gmail.com>
2026-04-04 14:02:37 +0300

    Reading and identifying group by cycles

--- src/main/java/ua/ihromant/mathutils/group/GapInteractor.java
+++ src/main/java/ua/ihromant/mathutils/group/GapInteractor.java
@@ -4,7 +4,9 @@ import tools.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class GapInteractor {
@@ -78,5 +80,73 @@ public class GapInteractor {
         }).findFirst().orElseThrow());
         process.destroyForcibly();
         return result;
+    }
+
+    public List<List<List<Integer>>> gapCycles(int order, int index) throws IOException {
+        process.outputWriter().write("IsPackageLoaded(\"LOOPS\");\n");
+        process.outputWriter().write("GeneratorsOfGroup(Image(IsomorphismPermGroup(SmallGroup(" + order + "," + index + "))));\n");
+        process.outputWriter().write("quit;\n");
+        process.outputWriter().flush();
+        String line = process.inputReader().lines().dropWhile(l -> !l.contains("false")).skip(1).map(l -> {
+            int idx = l.lastIndexOf(ANSI_RED);
+            String ln = idx < 0 ? l : l.substring(idx + ANSI_RED.length());
+            return ln.replace(" ", "");
+        }).collect(Collectors.joining());
+        process.destroyForcibly();
+        List<List<List<Integer>>> cycles = new ArrayList<>();
+        cycles.add(new ArrayList<>());
+        cycles.getLast().add(new ArrayList<>());
+        int from = findNext(line, 0);
+        int to = findNext(line, from);
+        char c;
+        while ((c = line.charAt(to)) != ']') {
+            cycles.getLast().getLast().add(Integer.parseInt(line.substring(from + 1, to)));
+            from = to;
+            to = findNext(line, from);
+            if (c == ')') {
+                char next = line.charAt(to);
+                if (next == ',') {
+                    cycles.add(new ArrayList<>());
+                    cycles.getLast().add(new ArrayList<>());
+                    from = to;
+                    to = findNext(line, from);
+                    from = to;
+                    to = findNext(line, from);
+                }
+                if (next == '(') {
+                    cycles.getLast().add(new ArrayList<>());
+                    from = to;
+                    to = findNext(line, from);
+                }
+            }
+        }
+        return cycles;
+    }
+
+    private static int findNext(String s, int from) {
+        for (int i = from + 1; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) {
+                return i;
+            }
+        }
+        return s.length();
+    }
+
+    public String identifyByCycles(List<List<List<Integer>>> cycles) throws IOException {
+        process.outputWriter().write("IsPackageLoaded(\"LOOPS\");\n");
+        String repr = cycles.stream().map(base -> {
+            return base.stream().map(l -> l.stream().map(Object::toString)
+                    .collect(Collectors.joining(",", "(", ")"))).collect(Collectors.joining());
+        }).collect(Collectors.joining(",", "[", "]"));
+        process.outputWriter().write("StructureDescription(Group(" + repr + "));\n");
+        process.outputWriter().write("quit;\n");
+        process.outputWriter().flush();
+        String joined = process.inputReader().lines().dropWhile(l -> !l.contains("false")).skip(1).map(l -> {
+            int idx = l.lastIndexOf(ANSI_RED);
+            return idx < 0 ? l : l.substring(idx + ANSI_RED.length());
+        }).collect(Collectors.joining());
+        process.destroyForcibly();
+        return joined;
     }
 }
--- src/main/java/ua/ihromant/mathutils/group/GroupIndex.java
+++ src/main/java/ua/ihromant/mathutils/group/GroupIndex.java
@@ -1,6 +1,12 @@
 package ua.ihromant.mathutils.group;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class GroupIndex {
     public static Group group(int order, int index) throws IOException {
@@ -24,5 +30,71 @@ public class GroupIndex {
 
     public static int groupCount(int order) throws IOException {
         return new GapInteractor().groupCount(order);
+    }
+
+    public static GapGroup gapGroup(int order, int idx) throws IOException {
+        List<List<List<Integer>>> cycles = new GapInteractor().gapCycles(order, idx);
+        int[][] gens = toGens(cycles);
+        return new GapGroup(permGroup(gens).asTable(), cycles);
+    }
+
+    public static String identify(GapGroup gg) throws IOException {
+        return new GapInteractor().identifyByCycles(gg.cycles());
+    }
+
+    private static int[][] toGens(List<List<List<Integer>>> cycles) {
+        int max = cycles.stream().flatMap(List::stream).flatMap(List::stream)
+                .mapToInt(Integer::intValue).max().orElseThrow();
+        List<int[]> perms = new ArrayList<>();
+        for (List<List<Integer>> base : cycles) {
+            int[] perm = IntStream.range(0, max).toArray();
+            for (List<Integer> cycle : base) {
+                for (int i = 0; i < cycle.size() - 1; i++) {
+                    perm[cycle.get(i) - 1] = cycle.get(i + 1) - 1;
+                }
+                perm[cycle.getLast() - 1] = cycle.getFirst() - 1;
+            }
+            perms.add(perm);
+        }
+        return perms.toArray(int[][]::new);
+    }
+
+    private static PermutationGroup permGroup(int[][] gens) {
+        Set<ArrWrap> result = new HashSet<>();
+        result.add(new ArrWrap(IntStream.range(0, gens[0].length).toArray()));
+        boolean added;
+        do {
+            added = false;
+            for (ArrWrap el : result.toArray(ArrWrap[]::new)) {
+                for (int[] gen : gens) {
+                    ArrWrap xy = new ArrWrap(combine(gen, el.map));
+                    ArrWrap yx = new ArrWrap(combine(el.map, gen));
+                    added = result.add(xy) || added;
+                    added = result.add(yx) || added;
+                }
+            }
+        } while (added);
+        return new PermutationGroup(result.stream().map(ArrWrap::map).toArray(int[][]::new));
+    }
+
+    private static int[] combine(int[] a, int[] b) {
+        int[] result = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            result[i] = a[b[i]];
+        }
+        return result;
+    }
+
+    private record ArrWrap(int[] map) {
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof ArrWrap(int[] map1))) return false;
+            return Arrays.equals(map, map1);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(map) >>> 1;
+        }
     }
 }
